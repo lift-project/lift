1f34a6ef9e3d483a827d3f80552bd9fd434bfa19
Michel Steuwer <michel.steuwer@uni-muenster.de>
2014-06-10 14:33:44 +0100

    Added ideas for integration of variables into expressions (a.k.a Fun)
    --HG--
    branch : generator

--- src/generators/Main.scala
+++ src/generators/Main.scala
@@ -31,5 +31,32 @@ object Main extends App {
   println(code)
   println("--------------------")
   
+  /*
+  val x = Variable("x", ArrayType(ScalarType("int"), Var("N")))
+  val y = Variable("y", ArrayType(ScalarType("int"), Var("N")))
+  val alpha = Variable("alpha", ScalarType("int"))
+  val beta = Variable("alpha", ScalarType("int"))
+  
+  val timesAlpha = UserFunc("int timesAlpha(int alpha, int x) { return alpha * x; }")
+  val add = UserFunc("int add(int x, int y) { return x+y; }")
+  val abs = UserFunc("int abs(int x) { x > 0 ? x : -x; }")
+  val mult = UserFunc("int mult(int x, int y) { return x* y; }")
+  
+  val scal1 = (alpha, x) => { Map(timesAlpha(alpha), x) }
+  val scal2 = (alpha, x) => { Map(timesAlpha(alpha)) o x }
+  
+  val asum1 = (x) => { Reduce(add, 0) o Map(abs, x) }
+  val asum2 = (x) => { Reduce(add, 0) o Map(abs) o x }
+  
+  val dot1 = (x, y) => { Reduce(add, 0) o Map(mult) o Zip(x, y) }
+  val dot2 = (x, y) => { Reduce(add, 0) o Map(mult) o Zip ??? }
+  
+  val gemv1 = (A, x, y, alpha, beta) => { Map(add) o Zip(Map(scal(alpha) o dot(x), A),
+		  				       				 		    scal(beta, y)) }
+  
+  val gemv2 = (A, x, y, alpha, beta) => { Map(add) o Zip(Map(scal(alpha) o dot(x)) o A,
+		  				       				 		    scal(beta, y)) }
+  */
+  
   println("end")
 }
