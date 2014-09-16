0c1f4b29fa7ea1e91b65ec4b30771c94fc49db4e
Michel Steuwer <michel.steuwer@uni-muenster.de>
2014-09-16 13:11:45 +0100

    Fixed up the JavaTest.

--- src/junit/opencl/generator/JavaTest.java
+++ src/junit/opencl/generator/JavaTest.java
@@ -87,28 +87,7 @@ public class JavaTest {
                     )).comp(jSplit.create(1024)).call(input);
                 }
         );
+
+        String code = Compile.apply(pairFun);
     }
-    /*
-    @Test def VECTOR_PAIR() {
-        val inputSize = 1024
-        val inputArray = Array.fill(inputSize)(util.Random.nextInt(5).toFloat)
-
-        val gold = inputArray.map((f) => Array(f, f)).flatten
-
-        val pair = UserFunDef("pair", "x", "{ Tuple t = {x, x}; return t; }", Float, TupleType(Float, Float))
-
-        val pairFun = fun(ArrayType(Float, Var("N")), (input) =>
-                Join() o MapWrg(
-                Join() o MapLcl(MapSeq(pair)) o Split(4)
-        ) o Split(1024) o input
-        )
-
-        val (output, runtime) = Execute(inputArray.length)(pairFun, inputArray, inputArray.size)
-
-        (gold, output).zipped.map(assertEquals(_,_,0.0))
-
-        println("output(0) = " + output(0))
-        println("runtime = " + runtime)
-    }
-    */
 }
