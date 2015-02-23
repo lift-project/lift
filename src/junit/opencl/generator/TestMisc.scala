106a9cdbe8717461c3231f9a343c1886d73686f7
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2015-02-23 17:35:31 +0000

    Added 2 more unittests for iterate, using local memory

--- src/junit/opencl/generator/TestMisc.scala
+++ src/junit/opencl/generator/TestMisc.scala
@@ -526,6 +526,44 @@ class TestMisc {
     assertArrayEquals(gold, output, 0.0f)
   }
 
+  @Test def iterateLocalRead(): Unit = {
+    val inputSize = 512
+    val input = Array.tabulate(inputSize)(_.toFloat)
+    val gold = input.map(_+1).map(_+1).map(_+1).map(_+1).map(_+1)
+
+    val f = fun(
+      ArrayType(Float, Var("N")),
+      in => Join() o MapWrg(toGlobal(Iterate(5)(MapLcl(plusOne))) o toLocal(MapLcl(id))) o Split(16) $ in
+    )
+
+    val (output, runtime) = Execute(inputSize)(f, input, inputSize)
+
+    println("output.size = " + output.size)
+    println("output(0) = " + output(0))
+    println("runtime = " + runtime)
+
+    assertArrayEquals(gold, output, 0.0f)
+  }
+
+  @Test def iterateLocalOnly(): Unit = {
+    val inputSize = 512
+    val input = Array.tabulate(inputSize)(_.toFloat)
+    val gold = input.map(_+1).map(_+1).map(_+1).map(_+1).map(_+1)
+
+    val f = fun(
+      ArrayType(Float, Var("N")),
+      in => Join() o MapWrg(toGlobal(MapLcl(id)) o Iterate(5)(MapLcl(plusOne)) o toLocal(MapLcl(id))) o Split(16) $ in
+    )
+
+    val (output, runtime) = Execute(inputSize)(f, input, inputSize)
+
+    println("output.size = " + output.size)
+    println("output(0) = " + output(0))
+    println("runtime = " + runtime)
+
+    assertArrayEquals(gold, output, 0.0f)
+  }
+
   @Test def localGlobalMemory(): Unit = {
     val inputSize = 512
     val input = Array.tabulate(inputSize)(_.toFloat)
