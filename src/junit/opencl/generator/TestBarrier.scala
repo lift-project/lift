46a56f2cd0e4a1ee8e20d698c5b8bfe04a928214
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2015-03-28 13:24:06 +0000

    Added a few unittests for barrier elimination
    --HG--
    branch : barriers

--- src/junit/opencl/generator/TestBarrier.scala
+++ src/junit/opencl/generator/TestBarrier.scala
@@ -2,8 +2,9 @@ package opencl.generator
 
 import ir.UserFunDef._
 import ir._
-import opencl.executor.{Execute, Executor}
+import opencl.executor.{Execute, Executor, Compile}
 import opencl.ir._
+import opencl.ir.IndexFunction._
 import org.junit.Assert._
 import org.junit.{AfterClass, BeforeClass, Test}
 
@@ -21,17 +22,58 @@ object TestBarrier {
 }
 
 class TestBarrier {
-  @Test def basicBarrier(): Unit ={
+  @Test def basicBarrier(): Unit = {
     val inputSize = 1024
     val input = Array.tabulate(inputSize)(_.toFloat)
 
+    // Barrier should be removed
     val f = fun(
       ArrayType(Float, new Var("N")),
       input => Join() o MapWrg(Barrier() o MapLcl(id)) o Split(128) $ input
     )
 
-    val (output, _) = Execute(inputSize)(f, input, inputSize)
+    val code = Compile(f)
+    val (output, _) = Execute(inputSize)(code, f, input, inputSize)
 
+    assertFalse(code.containsSlice("barrier"))
     assertArrayEquals(input, output, 0.0f)
   }
+
+  @Test def reorderGlobal(): Unit = {
+    val inputSize = 1024
+    val input = Array.tabulate(inputSize)(_.toFloat)
+    val gold = input.grouped(128).map(_.reverse).flatten.toArray
+
+    // All barriers should be removed
+    val f = fun(
+      ArrayType(Float, new Var("N")),
+      input => Join() o MapWrg(Barrier() o Gather(reverse)(MapLcl(id)) o Barrier() o MapLcl(id)) o Split(128) $ input
+    )
+
+    val code = Compile(f)
+    val (output, _) = Execute(inputSize)(code, f, input, inputSize)
+
+    assertFalse(code.containsSlice("barrier"))
+    assertArrayEquals(gold, output, 0.0f)
+  }
+
+  @Test def reorderLocal(): Unit = {
+    val inputSize = 1024
+    val input = Array.tabulate(inputSize)(_.toFloat)
+    val gold = input.grouped(128).map(_.reverse).flatten.toArray
+
+    // No barriers should be eliminated
+    val f = fun(
+      ArrayType(Float, new Var("N")),
+      input => Join() o MapWrg(Barrier() o toGlobal(Gather(reverse)(MapLcl(id))) o Barrier() o toLocal(MapLcl(id))) o Split(128) $ input
+    )
+
+    val code = Compile(f)
+    val (output, _) = Execute(inputSize)(code, f, input, inputSize)
+
+    assertEquals(2, "barrier".r.findAllMatchIn(code).length)
+    assertArrayEquals(gold, output, 0.0f)
+  }
+
+
 }
