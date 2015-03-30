ec7ad1039c06e69d64df95374feb47db37c24316
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2015-03-30 13:14:00 +0100

    Added some unittests for barrier elimination
    --HG--
    branch : barriers

--- src/junit/opencl/generator/TestBarrier.scala
+++ src/junit/opencl/generator/TestBarrier.scala
@@ -39,12 +39,12 @@ class TestBarrier {
     assertArrayEquals(input, output, 0.0f)
   }
 
-  @Test def reorderGlobal(): Unit = {
+  @Test def reorderGlobalLast(): Unit = {
     val inputSize = 1024
     val input = Array.tabulate(inputSize)(_.toFloat)
     val gold = input.grouped(128).map(_.reverse).flatten.toArray
 
-    // All barriers should be removed
+    // Last barrier should be removed
     val f = fun(
       ArrayType(Float, new Var("N")),
       input => Join() o MapWrg(Barrier() o Gather(reverse)(MapLcl(id)) o Barrier() o MapLcl(id)) o Split(128) $ input
@@ -57,7 +57,43 @@ class TestBarrier {
     assertArrayEquals(gold, output, 0.0f)
   }
 
+  @Test def reorderGlobalFirst(): Unit = {
+    val inputSize = 1024
+    val input = Array.tabulate(inputSize)(_.toFloat)
+    val gold = input.grouped(128).map(_.reverse).flatten.toArray
+
+    // All barriers should be removed
+    val f = fun(
+      ArrayType(Float, new Var("N")),
+      input => Join() o MapWrg(Barrier() o MapLcl(id) o Barrier() o Gather(reverse)(MapLcl(id))) o Split(128) $ input
+    )
+
+    val code = Compile(f)
+    val (output, _) = Execute(inputSize)(code, f, input, inputSize)
+
+    assertEquals(0, "barrier".r.findAllMatchIn(code).length)
+    assertArrayEquals(gold, output, 0.0f)
+  }
+
   @Test def reorderLocal(): Unit = {
+    val inputSize = 1024
+    val input = Array.tabulate(inputSize)(_.toFloat)
+    val gold = input.grouped(128).map(_.reverse).flatten.toArray
+
+    // First barrier should be eliminated
+    val f = fun(
+      ArrayType(Float, new Var("N")),
+      input => Join() o MapWrg(Barrier() o toGlobal(MapLcl(id)) o Barrier() o toLocal(Gather(reverse)(MapLcl(id)))) o Split(128) $ input
+    )
+
+    val code = Compile(f)
+    val (output, _) = Execute(inputSize)(code, f, input, inputSize)
+
+    assertEquals(1, "barrier".r.findAllMatchIn(code).length)
+    assertArrayEquals(gold, output, 0.0f)
+  }
+
+  @Test def reorderLastLocal(): Unit = {
     val inputSize = 1024
     val input = Array.tabulate(inputSize)(_.toFloat)
     val gold = input.grouped(128).map(_.reverse).flatten.toArray
@@ -75,5 +111,94 @@ class TestBarrier {
     assertArrayEquals(gold, output, 0.0f)
   }
 
+  @Test def reorderWriteLastLocal(): Unit = {
+    val inputSize = 1024
+    val input = Array.tabulate(inputSize)(_.toFloat)
+    val gold = input.grouped(128).map(_.reverse).flatten.toArray
 
+    // First barrier should be eliminated
+    val f = fun(
+      ArrayType(Float, new Var("N")),
+      input => Join() o MapWrg(Barrier() o toGlobal(Scatter(reverse)(MapLcl(id))) o Barrier() o toLocal(MapLcl(id))) o Split(128) $ input
+    )
+
+    val code = Compile(f)
+    val (output, _) = Execute(inputSize)(code, f, input, inputSize)
+
+    assertEquals(1, "barrier".r.findAllMatchIn(code).length)
+    assertArrayEquals(gold, output, 0.0f)
+  }
+
+
+  @Test def noReorderLocal(): Unit = {
+    val inputSize = 1024
+    val input = Array.tabulate(inputSize)(_.toFloat)
+    val gold = input
+
+    // First barrier should be eliminated
+    val f = fun(
+      ArrayType(Float, new Var("N")),
+      input => Join() o MapWrg(Barrier() o toGlobal(MapLcl(id)) o Barrier() o toLocal(MapLcl(id))) o Split(128) $ input
+    )
+
+    val code = Compile(f)
+    val (output, _) = Execute(inputSize)(code, f, input, inputSize)
+
+    assertEquals(1, "barrier".r.findAllMatchIn(code).length)
+    assertArrayEquals(gold, output, 0.0f)
+  }
+
+  @Test def noLoopNoReorderLocal(): Unit = {
+    val inputSize = 1024
+    val input = Array.tabulate(inputSize)(_.toFloat)
+    val gold = input
+
+    // All barriers should be eliminated
+    val f = fun(
+      ArrayType(Float, new Var("N")),
+      input => Join() o MapWrg(Barrier() o toGlobal(MapLcl(id)) o Barrier() o toLocal(MapLcl(id))) o Split(128) $ input
+    )
+
+    val code = Compile(f)
+    val (output, _) = Execute(128, inputSize, (true, true))(code, f, input, inputSize)
+
+    assertEquals(0, "barrier".r.findAllMatchIn(code).length)
+    assertArrayEquals(gold, output, 0.0f)
+  }
+
+  @Test def noLoopReorderLocal(): Unit = {
+    val inputSize = 1024
+    val input = Array.tabulate(inputSize)(_.toFloat)
+    val gold = input.grouped(128).map(_.reverse).flatten.toArray
+
+    // All barriers should be eliminated
+    val f = fun(
+      ArrayType(Float, new Var("N")),
+      input => Join() o MapWrg(Barrier() o toGlobal(MapLcl(id)) o Barrier() o toLocal(Gather(reverse)(MapLcl(id)))) o Split(128) $ input
+    )
+
+    val code = Compile(f)
+    val (output, _) = Execute(128, inputSize, (true, true))(code, f, input, inputSize)
+
+    assertEquals(0, "barrier".r.findAllMatchIn(code).length)
+    assertArrayEquals(gold, output, 0.0f)
+  }
+
+  @Test def noLoopReorderWriteLocal(): Unit = {
+    val inputSize = 1024
+    val input = Array.tabulate(inputSize)(_.toFloat)
+    val gold = input.grouped(128).map(_.reverse).flatten.toArray
+
+    // Last barrier should be eliminated
+    val f = fun(
+      ArrayType(Float, new Var("N")),
+      input => Join() o MapWrg(Barrier() o toGlobal(MapLcl(id)) o Barrier() o toLocal(Scatter(reverse)(MapLcl(id)))) o Split(128) $ input
+    )
+
+    val code = Compile(f)
+    val (output, _) = Execute(128, inputSize, (true, true))(code, f, input, inputSize)
+
+    assertEquals(1, "barrier".r.findAllMatchIn(code).length)
+    assertArrayEquals(gold, output, 0.0f)
+  }
 }
