685b34da97032a389b1e019e4b64a05fd2cf6552
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2015-04-03 10:41:23 +0100

    Changed the expression for matrix transposition again, so the output type would be correct

--- src/benchmarks/MatrixTransposition.scala
+++ src/benchmarks/MatrixTransposition.scala
@@ -43,7 +43,8 @@ object MatrixTransposition {
     ArrayType(ArrayType(Float, M), N),
     (matrix) => {
       // Merge the tiles
-      Join() o MapWrg(0)(Join() o TransposeW() o MapWrg(1)(Barrier() o toGlobal(MapLcl(1)(MapLcl(0)(id))) o
+      Join() o MapWrg(0)(Map(Join()) o TransposeW() o MapWrg(1)(
+        Barrier() o toGlobal(MapLcl(1)(MapLcl(0)(id))) o
         // Transpose the tiles and then the insides of tiles
         TransposeW() o Barrier() o toLocal(MapLcl(1)(MapLcl(0)(id)))
       )) o Transpose() o
