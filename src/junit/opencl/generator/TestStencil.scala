3aba919b60a9d78c39b56c45c32c9a62cbb430e5
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2015-04-07 15:14:04 +0100

    Cleared code style warnings
    --HG--
    branch : stencils

--- src/junit/opencl/generator/TestStencil.scala
+++ src/junit/opencl/generator/TestStencil.scala
@@ -84,7 +84,7 @@ class TestStencil {
   }
 
   @Test def SIMPLE_GROUP_1D_STENCIL(): Unit = {
-    val data = Seq.fill(1024)(Random.nextFloat).toArray
+    val data = Seq.fill(1024)(Random.nextFloat()).toArray
     val weights = Array(1, 2, 5, 2, 1) map {_ / 11.0f}
     val relativeIndices = Array(-2, -1, 0, 1, 2)
 
@@ -100,8 +100,8 @@ class TestStencil {
       }
     )
 
-    val (output, runtime) = Execute(data.size)(stencilFun, data, weights, data.size, weights.size)
-    println("output.size = " + output.size)
+    val (output, runtime) = Execute(data.length)(stencilFun, data, weights, data.length, weights.length)
+    println("output.length = " + output.length)
     println("output(0) = " + output(0))
     println("runtime = " + runtime)
 
@@ -109,7 +109,7 @@ class TestStencil {
   }
 
   @Test def GROUP_DOTPRODUCT_1D_STENCIL(): Unit = {
-    val data = Seq.fill(1024)(Random.nextFloat).toArray
+    val data = Seq.fill(1024)(Random.nextFloat()).toArray
     val weights = Array(1, 2, 5, 2, 1) map {_ / 11.0f}
     val relativeIndices = Array(-2, -1, 0, 1, 2)
 
@@ -125,8 +125,8 @@ class TestStencil {
       }
     )
 
-    val (output, runtime) = Execute(data.size)(stencilFun, data, weights, data.size, weights.size)
-    println("output.size = " + output.size)
+    val (output, runtime) = Execute(data.length)(stencilFun, data, weights, data.length, weights.length)
+    println("output.length = " + output.length)
     println("output(0) = " + output(0))
     println("runtime = " + runtime)
 
@@ -136,7 +136,7 @@ class TestStencil {
   @Test def GROUP2D_DOTPRODUCT_2D_STENCIL(): Unit = {
     val Nsize = 128
     val Msize = 128
-    val matrix = Array.tabulate(Nsize, Msize)((r, c) => Random.nextFloat)
+    val matrix = Array.tabulate(Nsize, Msize)((r, c) => Random.nextFloat())
     val relColumns = Array(-1, 0, 1)
     val relRows = Array(-1, 0, 1)
     val weights = Array(0.08f, 0.12f, 0.08f, 0.12f, 0.2f, 0.12f, 0.08f, 0.12f, 0.08f)
@@ -157,7 +157,7 @@ class TestStencil {
 
     val (output, runtime) = Execute(16, 16, Nsize, Msize, (false, false))(f, matrix, weights, Nsize, Msize, weights.length)
 
-    println("output.size = " + output.size)
+    println("output.length = " + output.length)
     println("output(0) = " + output(0))
     println("runtime = " + runtime)
 
@@ -174,8 +174,8 @@ class TestStencil {
       (input) =>
         MapGlb(MapSeq(id)) o Group(relIndices, Group.edgeNeg, Group.edgePos) $ input
     )
-    val (output, runtime) = Execute(1, data.size)(f, data, data.size)
-    println("output.size = " + output.size)
+    val (output, runtime) = Execute(1, data.length)(f, data, data.length)
+    println("output.length = " + output.length)
     println("output(0) = " + output(0))
     println("runtime = " + runtime)
 
@@ -192,8 +192,8 @@ class TestStencil {
       (input) =>
         MapGlb(MapSeq(id)) o Group(relIndices, Group.reflectNeg, Group.reflectPos) $ input
     )
-    val (output, runtime) = Execute(1, data.size)(f, data, data.size)
-    println("output.size = " + output.size)
+    val (output, runtime) = Execute(1, data.length)(f, data, data.length)
+    println("output.length = " + output.length)
     println("output(0) = " + output(0))
     println("runtime = " + runtime)
 
@@ -210,8 +210,8 @@ class TestStencil {
       (input) =>
         MapGlb(MapSeq(id)) o Group(relIndices, Group.wrapNeg, Group.wrapPos) $ input
     )
-    val (output, runtime) = Execute(1, data.size)(f, data, data.size)
-    println("output.size = " + output.size)
+    val (output, runtime) = Execute(1, data.length)(f, data, data.length)
+    println("output.length = " + output.length)
     println("output(0) = " + output(0))
     println("runtime = " + runtime)
 
