32d6eb8bad53bce503b2b66e81458fc1ca13ffa1
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2015-04-08 11:31:04 +0100

    Moved more java interop stuff into a separate package.

--- src/opencl/ir/Type.scala
+++ src/opencl/ir/Type.scala
@@ -17,17 +17,3 @@ object Float3 extends VectorType(Float, Cst(3))
 object Float4 extends VectorType(Float, Cst(4))
 object Float8 extends VectorType(Float, Cst(8))
 object Float16 extends VectorType(Float, Cst(16))
-
-object jInt { def getSingleton = Int }
-object jInt2 { def getSingleton = Int2 }
-object jInt3 { def getSingleton = Int3 }
-object jInt4 { def getSingleton = Int4 }
-object jInt8 { def getSingleton = Int8 }
-object jInt16 { def getSingleton = Int16 }
-
-object jFloat { def getSingleton = Float }
-object jFloat2 { def getSingleton = Float2 }
-object jFloat3 { def getSingleton = Float3 }
-object jFloat4 { def getSingleton = Float4 }
-object jFloat8 { def getSingleton = Float8 }
-object jFloat16 { def getSingleton = Float16 }
