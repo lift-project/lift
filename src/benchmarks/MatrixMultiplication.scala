dfc9a85dba00def4ed67f404a559b160e1e4baf8
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2015-04-16 17:01:50 +0100

    Added a benchmark for matrix multiplication
    --HG--
    branch : reduce

---
+++ src/benchmarks/MatrixMultiplication.scala
@@ -0,0 +1,133 @@
+package benchmarks
+
+import arithmetic.Var
+import ir.UserFunDef._
+import ir._
+import opencl.ir.CompositePatterns._
+import opencl.ir._
+import org.clapper.argot.ArgotConverters._
+
+class MatrixMultiplication (override val f: Seq[(String, Array[Lambda])])
+  extends Benchmark("Matrix Multiplication", Seq(1024, 1024, 1024), f, 0.001f, Array(16, 16, 1)) {
+
+  val tileX = parser.option[Int](List("x", "tileX"), "size",
+    "Tile size in the x dimension")
+
+  val tileY = parser.option[Int](List("y", "tileY"), "size",
+    "Tile size in the y dimension")
+
+  override def runScala(inputs: Any*): Array[Float] = {
+    val A = inputs(0).asInstanceOf[Array[Array[Float]]]
+    val B = inputs(1).asInstanceOf[Array[Array[Float]]]
+
+    val aCols = A(0).length
+    val aRows = A.length
+    val bCols = B(0).length
+    val res =  Array.ofDim[Float](aRows, bCols)
+
+    @inline def computeRow(row: Int) {
+      // while statements are much faster than for statements
+      var col = 0
+      while(col < bCols) { var i = 0; var sum = 0.0f
+        while(i < aCols) {
+          sum += A(row)(i) * B(col)(i)
+          i += 1
+        }
+
+        res(row)(col) = sum
+        col += 1
+      }
+    }
+
+    (0 until aRows).par.foreach( computeRow )
+
+    res.flatten
+  }
+
+  override def generateInputs(): Seq[Any] = {
+    val inputSizeN = inputSizes()(0)
+    val inputSizeM = inputSizes()(1)
+    val inputSizeK = inputSizes()(2)
+
+    val matrixA = Array.tabulate(inputSizeK, inputSizeM)((r, c) => (((r * 3 + c * 2) % 10) + 1) * 0.1f)
+    val matrixB = Array.tabulate(inputSizeK, inputSizeN)((r, c) => (((r * 3 + c * 2) % 10) + 1) * 0.1f)
+
+    Seq(matrixA, matrixB)
+  }
+
+  override def globalSize: Array[Int] = {
+    val globalSizes = Array(inputSizes()(0), inputSizes()(1), 1)
+    globalSizeOpt.value.copyToArray(globalSizes)
+    globalSizes
+  }
+
+  override protected def beforeBenchmark() = {
+    f(1)._2(0) = MatrixMultiplication.coalesced(tileX.value.getOrElse(16))
+  }
+}
+
+object MatrixMultiplication {
+  val N = Var("N")
+  val M = Var("M")
+  val K = Var("K")
+
+  val naive = fun(
+    ArrayType(ArrayType(Float, K), M),
+    ArrayType(ArrayType(Float, K), N),
+    (A, B) => {
+      MapWrg(fun( Arow =>
+        Barrier() o MapLcl(fun( Bcol =>
+          toGlobal(MapSeq(id)) o ReduceSeq(fun((acc, y) => multAndSumUp.apply(acc, Get(y, 0), Get(y, 1))), 0.0f) $ Zip(Arow, Bcol)
+        )) $ B
+      )) $ A
+    })
+
+  def coalesced(tileSize: Int = 4) = fun(
+    ArrayType(ArrayType(Float, K), M),
+    ArrayType(ArrayType(Float, K), N),
+    (A, B) => {
+      // Undo the tiling
+      Untile() o
+        MapWrg(0)(fun( aRows =>
+          MapWrg(1)(fun( bCols =>
+
+            toGlobal(MapLcl(0)(MapLcl(1)(id))) o
+              Join() o
+
+              // Multiply all necessary combinations of tiles
+              ReduceSeq(fun( (acc, pairOfTiles) =>
+
+                fun(pairOfTiles =>
+                  Barrier() o fun(partial => MapLcl(0)(fun(pairOfRows => MapLcl(1)(add) $ Zip(Get(pairOfRows, 0), Get(pairOfRows, 1)))) $ Zip(acc, partial) ) o
+                    Map(Join()) o
+                    MapLcl(0)( fun(rowA =>
+                      MapLcl(1)( fun( colB =>
+                        toLocal(MapSeq(id) o ReduceSeq(fun((acc, y) => multAndSumUp.apply(acc, Get(y, 0), Get(y, 1))), 0.0f)) $ Zip(rowA, colB)
+                      )) $ Get(pairOfTiles, 1)
+                    )) $ Get(pairOfTiles, 0)
+                ) o
+
+                  // Copy tiles to local memory
+                  fun(pairOfTiles =>
+                    Tuple(
+                      Barrier() o toLocal(MapLcl(0)(MapLcl(1)(id))) $ Get(pairOfTiles, 0),
+                      Barrier() o toLocal(MapLcl(0)(MapLcl(1)(id))) $ Get(pairOfTiles, 1)
+                    )) $ pairOfTiles
+              )
+                , toLocal(MapLcl(0)(MapLcl(1)(id))) $ Value(0.0f, ArrayType(ArrayType(Float, tileSize), tileSize))
+              ) $ Zip(aRows, bCols)
+
+            // Tile the matrices
+          )) o Tile(tileSize) $ B
+        )) o Tile(tileSize) $ A
+    })
+
+  def apply() = new MatrixMultiplication(
+    Seq(("naive", Array[Lambda](naive)),
+      ("coalesced", Array[Lambda](coalesced(16)))
+    ))
+
+  def main(args: Array[String]): Unit = {
+    MatrixMultiplication().run(args)
+  }
+}
--- src/junit/opencl/generator/TestMatrixMatrix.scala
+++ src/junit/opencl/generator/TestMatrixMatrix.scala
@@ -1,6 +1,7 @@
 package opencl.generator
 
 import arithmetic.Var
+import benchmarks.MatrixMultiplication
 import opencl.executor._
 import org.junit.Assert._
 import org.junit.{AfterClass, BeforeClass, Test, Ignore}
@@ -1078,49 +1079,7 @@ class TestMatrixMatrix {
 
     val gold = matrixMatrixMultiply(matrixA, matrixB).flatten
 
-    val n = new Var("N")
-    val m = new Var("M")
-    val k = new Var("K")
-
-    val f = fun(
-      ArrayType(ArrayType(Float, k), m),
-      ArrayType(ArrayType(Float, k), n),
-      (A, B) => {
-        // Undo the tiling
-        Untile() o
-          MapWrg(0)(fun( aRows =>
-            MapWrg(1)(fun( bCols =>
-
-                toGlobal(MapLcl(0)(MapLcl(1)(id))) o
-                  Join() o
-
-                // Multiply all necessary combinations of tiles
-                ReduceSeq(fun( (acc, pairOfTiles) =>
-
-                  fun(pairOfTiles =>
-                  Barrier() o fun(partial => MapLcl(0)(fun(pairOfRows => MapLcl(1)(add) $ Zip(Get(pairOfRows, 0), Get(pairOfRows, 1)))) $ Zip(acc, partial) ) o
-                    Map(Join()) o
-                  MapLcl(0)( fun(rowA =>
-                    MapLcl(1)( fun( colB =>
-                      toLocal(MapSeq(id) o ReduceSeq(fun((acc, y) => multAndSumUp.apply(acc, Get(y, 0), Get(y, 1))), 0.0f)) $ Zip(rowA, colB)
-                    )) $ Get(pairOfTiles, 1)
-                  )) $ Get(pairOfTiles, 0)
-                ) o
-
-                  // Copy tiles to local memory
-                  fun(pairOfTiles =>
-                    Tuple(
-                      Barrier() o toLocal(MapLcl(0)(MapLcl(1)(id))) $ Get(pairOfTiles, 0),
-                      Barrier() o toLocal(MapLcl(0)(MapLcl(1)(id))) $ Get(pairOfTiles, 1)
-                    )) $ pairOfTiles
-                )
-                , toLocal(MapLcl(0)(MapLcl(1)(id))) $ Value(0.0f, ArrayType(ArrayType(Float, tileSize), tileSize))
-                ) $ Zip(aRows, bCols)
-
-              // Tile the matrices
-            )) o Tile(tileSize) $ B
-          )) o Tile(tileSize) $ A
-      })
+    val f = MatrixMultiplication.coalesced(tileSize)
 
     val (output, runtime) = Execute(mSize * nSize)(f, matrixA, matrixB.transpose, mSize, kSize, nSize)
 
