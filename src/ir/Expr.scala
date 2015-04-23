524f73a59f93c51f931e57d649d78b97da9d7b5e
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2015-04-23 11:28:41 +0100

    Added a workaround for compound literals for AMD OpenCL SDK
    --HG--
    branch : compoundLiterals

--- src/ir/Expr.scala
+++ src/ir/Expr.scala
@@ -209,12 +209,12 @@ object Value {
 
   implicit def Tuple2ToValue[T1, T2](t: (T1, T2)): Value = {
     val tupleType = TupleType(getType(t._1), getType(t._2))
-    Value("(" + Type.name(tupleType) + ")" + t.toString().replace('(', '{').replace(')', '}'), tupleType)
+    Value(t.toString().replace('(', '{').replace(')', '}'), tupleType)
   }
 
   implicit def Tuple3ToValue[T1, T2, T3](t: (T1, T2, T3)): Value = {
     val tupleType = TupleType(getType(t._1), getType(t._2), getType(t._3))
-    Value("(" + Type.name(tupleType) + ")" + t.toString().replace('(', '{').replace(')', '}'), tupleType)
+    Value(t.toString().replace('(', '{').replace(')', '}'), tupleType)
   }
 
   private def getType(a: Any): Type = a match {
