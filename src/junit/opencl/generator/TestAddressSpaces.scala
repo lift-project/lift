54835f8853128379ff3c43901a268f33753361d2
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2015-04-24 14:54:41 +0100

    Added a few tests when 3 address spaces are used
    --HG--
    branch : toPrivate

--- src/junit/opencl/generator/TestAddressSpaces.scala
+++ src/junit/opencl/generator/TestAddressSpaces.scala
@@ -6,7 +6,7 @@ import ir.{Split, Join, ArrayType, fun}
 import opencl.executor.{Executor, Compile, Execute}
 import opencl.ir._
 import org.junit.Assert._
-import org.junit.{Ignore, AfterClass, BeforeClass, Test}
+import org.junit.{AfterClass, BeforeClass, Test}
 
 object TestAddressSpaces {
   @BeforeClass def before() {
@@ -157,6 +157,52 @@ class TestAddressSpaces {
       ArrayType(Float, Var("N")),
       in => Join() o MapWrg( Join() o MapLcl(toGlobal(MapUnroll(id)) o toPrivate(MapUnroll(plusOne))) o Split(4))
         o Split(128) $ in
+    )
+
+    val (output, runtime) = Execute(inputSize)(f, input, inputSize)
+
+    println("output.size = " + output.length)
+    println("output(0) = " + output(0))
+    println("runtime = " + runtime)
+
+    assertArrayEquals(gold, output, 0.0f)
+  }
+
+  @Test def privateLocalGlobal(): Unit = {
+    val inputSize = 512
+    val input = Array.tabulate(inputSize)(_.toFloat)
+
+    val gold = input.map(_+1)
+
+    val  f = fun(
+      ArrayType(Float, Var("N")),
+      in => Join() o MapWrg( toGlobal(MapLcl(id)) o Join() o Barrier() o
+        MapLcl(toLocal(MapUnroll(id)) o toPrivate(MapUnroll(plusOne))) o Split(4) o
+        Barrier() o toLocal(MapLcl(id))
+      ) o Split(128) $ in
+    )
+
+    val (output, runtime) = Execute(inputSize)(f, input, inputSize)
+
+    println("output.size = " + output.length)
+    println("output(0) = " + output(0))
+    println("runtime = " + runtime)
+
+    assertArrayEquals(gold, output, 0.0f)
+  }
+
+  @Test def privateLocalGlobal2(): Unit = {
+    val inputSize = 512
+    val input = Array.tabulate(inputSize)(_.toFloat)
+
+    val gold = input.map(_+1)
+
+    val  f = fun(
+      ArrayType(Float, Var("N")),
+      in => Join() o MapWrg( Join() o
+        MapLcl(toGlobal(MapUnroll(id)) o toPrivate(MapUnroll(plusOne))) o Split(4) o
+        Barrier() o toLocal(MapLcl(id))
+      ) o Split(128) $ in
     )
 
     val (output, runtime) = Execute(inputSize)(f, input, inputSize)
