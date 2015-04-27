eed87d559545bbad87f1d25e8e3f43f83abc600c
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2015-04-27 15:23:59 +0100

    Added unittests for a few problems with private memory
    --HG--
    branch : toPrivate

--- src/junit/opencl/generator/TestAddressSpaces.scala
+++ src/junit/opencl/generator/TestAddressSpaces.scala
@@ -6,7 +6,7 @@ import ir._
 import opencl.executor.{Executor, Compile, Execute}
 import opencl.ir._
 import org.junit.Assert._
-import org.junit.{AfterClass, BeforeClass, Test}
+import org.junit.{Ignore, AfterClass, BeforeClass, Test}
 
 object TestAddressSpaces {
   @BeforeClass def before() {
@@ -209,5 +209,66 @@ class TestAddressSpaces {
 
     println("output(0) = " + output(0))
     println("runtime = " + runtime)
+  }
+
+  @Ignore
+  @Test def initArrayValuePrivate(): Unit = {
+    val inputSize = 512
+    val inputData = Array.fill(inputSize)(util.Random.nextInt(5).toFloat)
+
+    val l = fun (ArrayType(Float, Var("N")), (in) => {
+      Join() o MapWrg( fun(x =>
+        toGlobal(MapLcl(id)) o MapLcl(toPrivate(id)) $ Value("0.0f", ArrayType(Float, 4)))
+      ) o Split(4) $ in
+    })
+
+    val (output, runtime) = Execute(inputData.length)( l, inputData, inputData.length )
+
+    assertEquals(0.0f, output.sum, 0.0)
+
+    println("output(0) = " + output(0))
+    println("runtime = " + runtime)
+  }
+
+  @Ignore
+  @Test def initArrayValuePrivate2(): Unit = {
+    val inputSize = 512
+    val inputData = Array.fill(inputSize)(util.Random.nextInt(5).toFloat)
+
+    val l = fun (ArrayType(Float, Var("N")), (in) => {
+      Join() o MapGlb( fun(x =>
+        toGlobal(MapSeq(id)) o MapSeq(id) $ Value("0.0f", ArrayType(Float, 4)))
+      ) o Split(4) $ in
+    })
+
+    val (output, runtime) = Execute(inputData.length)( l, inputData, inputData.length )
+
+    assertEquals(0.0f, output.sum, 0.0)
+
+    println("output(0) = " + output(0))
+    println("runtime = " + runtime)
+  }
+
+  @Ignore
+  @Test def privateGlobalMemory2(): Unit = {
+    val inputSize = 512
+    val input = Array.tabulate(inputSize)(_.toFloat)
+
+    val gold = input.map(_+1)
+
+    val  f = fun(
+      ArrayType(Float, Var("N")),
+      in => Join() o MapWrg(Join() o Barrier() o MapLcl(toGlobal(MapSeq(id)))
+        o MapLcl(toPrivate(MapSeq(plusOne))) o Split(1))
+        o Split(4) $ in
+    )
+
+    val (output, runtime) = Execute(inputSize)(f, input, inputSize)
+
+    println("output.size = " + output.length)
+    println("output(0) = " + output(0))
+    println("runtime = " + runtime)
+
+    assertArrayEquals(gold, output, 0.0f)
   }
 }
