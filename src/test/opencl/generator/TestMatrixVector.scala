92097ad69b83b0217d2cca209d90915d9e1c011d
Thibaut Lutz <thibaut.lutz@gmail.com>
2015-05-27 10:24:38 +0100

    Re-disabling the MATRIX_VECTOR_LOCAL_MEMORY test until we support device property queries.

--- src/test/opencl/generator/TestMatrixVector.scala
+++ src/test/opencl/generator/TestMatrixVector.scala
@@ -142,7 +142,10 @@ class TestMatrixVector {
     assertArrayEquals(matrixVector(matrix, vector), output, 0.0f)
   }
 
+  @Ignore
   @Test def MATRIX_VECTOR_LOCAL_MEMORY() {
+
+    // TODO(tlutz) This is a memory hungry test. It is disabled until we can query the device properties.
 
     val inputSize = 2048
     val matrix = Array.tabulate(inputSize, inputSize)((r,c) => 1.0f)
