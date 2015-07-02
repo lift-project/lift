916eb551d51c62ac43d76198106fc518666534e7
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2015-07-02 16:01:29 +0100

    Added the possibility to restart the experiment from the point where it crashed

--- src/main/exploration/MultiplicationParameters.scala
+++ src/main/exploration/MultiplicationParameters.scala
@@ -4,7 +4,10 @@ import scala.sys.process._
 
 object MultiplicationParameters {
 
+  val separator = "------------------------------"
+
   def main(args: Array[String]) : Unit = {
+
     val platform = 0
     val device = 0
     val variant = 3
@@ -16,13 +19,22 @@ object MultiplicationParameters {
     val threshold = 16
     var counter = 0
 
-    val baseline = s"--il -s $nSize -s $mSize -s $kSize -p $platform -d $device -x128 -y16 --bm 8 --bn 8 -l16 -l16 --variant 3 -g128 -g128 -c"
+    val baseline = s"--ig --il -s $nSize -s $mSize -s $kSize -p $platform -d $device -x128 -y16 --bm 8 --bn 8 -l16 -l16 --variant 3 -g128 -g128 -c"
     val script = "scripts/MatrixMultiplication"
+
+    var doRecovery = false
+    var filename = ""
+
+    if (args.nonEmpty) {
+      doRecovery = true
+      filename = args(0)
+    }
 
     var tileSizeK = 2
     while (tileSizeK <= 2048) {
 
       var tileSizeM = 2
+
       var tileSizeN = tileSizeM
       while (tileSizeM <= 2048) {
 
@@ -38,8 +50,8 @@ object MultiplicationParameters {
               if (workPerThreadM * workPerThreadN <= 128 &&
                 tileSizeM >= workPerThreadM && tileSizeN >= workPerThreadN ) {
 
-                val localSizeM = tileSizeM / workPerThreadM
-                val localSizeN = tileSizeN / workPerThreadN
+                var localSizeM = tileSizeM / workPerThreadM
+                var localSizeN = tileSizeN / workPerThreadN
 
                 if (localSizeM*localSizeN <= 1024) {
 
@@ -51,25 +63,42 @@ object MultiplicationParameters {
 
                       if (globalSizeM >= localSizeM && globalSizeN >= localSizeN) {
 
-                        val parameters = Array("--il", "-s" + mSize, "-s" + kSize, "-s" + nSize,
-                          "-p" + platform, "-d" + device, "-x" + tileSizeM, "-y" + tileSizeK,
+                        if (doRecovery) {
+                          recover(filename) match {
+                            case (x, y, bm, bn, l1, l2, g1, g2) =>
+                              tileSizeM = x
+                              tileSizeK = y
+                              workPerThreadM = bm
+                              workPerThreadN = bn
+                              localSizeM = l1
+                              localSizeN = l2
+                              globalSizeM = g1
+                              globalSizeN = g2
+                            case _ =>
+                          }
+                          doRecovery = false
+                        }
+
+                        val parameters = Array("--il", "-c", "-s" + mSize, "-s" + kSize, "-s" + nSize,
+                          "-p" + platform, "-d" + device, "--variant", variant.toString,
+                          "-x" + tileSizeM, "-y" + tileSizeK,
                           "--bm", workPerThreadM.toString, "--bn", workPerThreadN.toString,
                           "-l" + localSizeM, "-l" + localSizeN,
-                          "--variant", variant.toString, "-g" + globalSizeM, "-g" + globalSizeN, "-c").mkString(" ")
+                          "-g" + globalSizeM, "-g" + globalSizeN).mkString(" ")
 
                         if (counter % threshold == 0) {
                           counter = threshold
-                          println("------------------------------")
+                          println(separator)
                           println("baseline")
                           (script + " " + baseline).!
                         }
 
                         counter += 1
 
-                        println("------------------------------")
+                        println(separator)
                         println(parameters)
                         (script + " " + parameters).!
-                        println("------------------------------")
+                        println(separator)
                         val s = parameters + " --ig"
                         println(s)
                         (script + " " + s).!
@@ -92,6 +121,28 @@ object MultiplicationParameters {
         tileSizeN = tileSizeM
       }
       tileSizeK *= 2
+    }
+  }
+
+  def recover(filename: String): (Int, Int, Int, Int, Int, Int, Int, Int) = {
+    var log = s"tail -n 125 $filename".!!
+
+    var index = -1
+    var findHere = Array.empty[String]
+    var lastConf = ""
+
+    do {
+      index = log.lastIndexOf(separator)
+      findHere = log.substring(index).split("\n")
+      lastConf = findHere(1)
+      log = log.substring(0, index)
+    } while(lastConf.startsWith("baseline"))
+
+    val pattern = """.*-x(\d+) -y(\d+) --bm (\d+) --bn (\d+) -l(\d+) -l(\d+) -g(\d+) -g(\d+).*""".r
+
+    lastConf match {
+      case pattern(x, y, bm, bn, l1, l2, g1, g2) =>
+        (x.toInt, y.toInt, bm.toInt, bn.toInt, l1.toInt, l2.toInt, g1.toInt, g2.toInt)
     }
   }
 
