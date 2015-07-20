d34da4dc4c32c461098e6e40331ab964b9441e30
Michel Steuwer <michel.steuwer@ed.ac.uk>
2015-07-20 10:20:05 +0100

    Minor refactoring

--- src/main/ir/ast/UserFun.scala
+++ src/main/ir/ast/UserFun.scala
@@ -2,7 +2,6 @@ package ir.ast
 
 import arithmetic.ArithExpr
 import ir.{TupleType, VectorType, ScalarType, Type}
-import opencl.ir.{Int, Float}
 
 /**
  * Representation of a "user function" declaration which usually operates on scala values.
@@ -133,46 +132,5 @@ object UserFun {
 
   @deprecated("replaced by UserFun.vectorize(n)")
   def vectorize(uf: UserFun, n: ArithExpr): UserFun = uf.vectorize(n)
-
-  // commonly used user functions
-
-  val id = UserFun("id", "x", "{ return x; }", Float, Float)
-
-  val idI = UserFun("id", "x", "{ return x; }", Int, Int)
-
-  val idFI = UserFun("id", "x", "{ return x; }", TupleType(Float, Int), TupleType(Float, Int))
-
-  val idFF = UserFun("id", "x", "{ return x; }", TupleType(Float, Float), TupleType(Float, Float))
-
-  val absAndSumUp = UserFun("absAndSumUp", Array("acc", "x"), "{ return acc + fabs(x); }",
-                            Seq(Float, Float), Float)
-
-  val add = UserFun("add", Array("x", "y"), "{ return x+y; }", Seq(Float, Float), Float)
-
-  val plusOne = UserFun("plusOne", "x", "{ return x+1; }", Float, Float)
-
-  val doubleItAndSumUp = UserFun("doubleItAndSumUp", Array("x", "y"), "{ return x + (y * y); }",
-                                 Seq(Float, Float), Float)
-
-  val sqrtIt = UserFun("sqrtIt", "x", "{ return sqrt(x); }", Float, Float)
-
-  val abs = UserFun("abs", "x", "{ return x >= 0 ? x : -x; }", Float, Float)
-
-  val neg = UserFun("neg", "x", "{ return -x; }", Float, Float)
-
-  val mult = UserFun("mult", Array("l", "r"), "{ return l * r; }", Seq(Float, Float), Float)
-
-  val multAndSumUp = UserFun("multAndSumUp", Array("acc", "l", "r"),
-                                "{ return acc + (l * r); }",
-                                Seq(Float, Float, Float), Float)
-
-  val addPair = UserFun(
-                            "pair",
-                            Array("x", "y"),
-                            "{ x._0 = x._0 + y._0;" +
-                            "x._1 = x._1 + y._1;" +
-                            "return x; }",
-                            Seq(TupleType(Float, Float), TupleType(Float, Float)),
-                            TupleType(Float, Float))
 
 }
