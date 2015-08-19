de7f125a56530eb16a2a6095d98c4db87cd29b91
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2015-08-19 14:23:53 +0100

    Revert "Slightly improved the error message for Reduce typechecking"
    This reverts commit acebd723bf63f9ee1ba2949251b3a73a005e58bc.

--- src/main/ir/ast/Zip.scala
+++ src/main/ir/ast/Zip.scala
@@ -34,8 +34,8 @@ case class Zip(n : Int) extends FunDecl(arity = n) with isGenerable {
 
         // make sure all arguments have the same size
         if (arrayTypes.map(_.len).distinct.length != 1) {
-          throw new TypeException("Warning: can not statically prove that sizes (" +
-            tt.elemsT.mkString(", ") + ") match!\n")
+          Console.err.println("Warning: can not statically prove that sizes (" +
+            tt.elemsT.mkString(", ") + ") match!")
           // throw TypeException("sizes do not match")
         }
 
