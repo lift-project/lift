b1a229d8156b23c2819b47c6d8b9b9b68b8f7741
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2015-10-09 13:55:40 +0100

    Changing the names of userfunctions on doubles.

--- src/main/opencl/ir/package.scala
+++ src/main/opencl/ir/package.scala
@@ -12,7 +12,7 @@ package object ir {
 
   val idI = UserFun("id", "x", "{ return x; }", Int, Int)
 
-  val iddouble = UserFun("id", "x", "{ return x; }", Double, Double)
+  val iddouble = UserFun("iddouble", "x", "{ return x; }", Double, Double)
 
   val idFI = UserFun("id", "x", "{ return x; }", TupleType(Float, Int), TupleType(Float, Int))
 
@@ -23,7 +23,7 @@ package object ir {
 
   val add = UserFun("add", Array("x", "y"), "{ return x+y; }", Seq(Float, Float), Float)
 
-  val adddouble = UserFun("add", Array("x", "y"), "{ return x+y; }", Seq(Double, Double), Double)
+  val adddouble = UserFun("adddouble", Array("x", "y"), "{ return x+y; }", Seq(Double, Double), Double)
 
   val plusOne = UserFun("plusOne", "x", "{ return x+1; }", Float, Float)
 
@@ -38,7 +38,7 @@ package object ir {
 
   val mult = UserFun("mult", Array("l", "r"), "{ return l * r; }", Seq(Float, Float), Float)
 
-  val multdouble = UserFun("mult", Array("l", "r"), "{ return l * r; }", Seq(Double, Double), Double)
+  val multdouble = UserFun("multdouble", Array("l", "r"), "{ return l * r; }", Seq(Double, Double), Double)
 
   val multAndSumUp = UserFun("multAndSumUp", Array("acc", "l", "r"),
                              "{ return acc + (l * r); }",
