243a4d2787a8c82efe207fc9d7067adee5173e17
Michel Steuwer <michel.steuwer@ed.ac.uk>
2015-10-31 15:47:10 +0000

    Added version of mv and mm in an data flow / imperative style

--- src/main/benchmarks/MatrixVector.scala
+++ src/main/benchmarks/MatrixVector.scala
@@ -72,10 +72,10 @@ object MatrixVector {
     MapWrg(
       fun(t =>
         Zip(
-          Zip(vectorX, Get(t, 0)) :>>
+          Zip(vectorX, t._0) :>>
           Split(N) :>>
           toLocal(MapLcl(
-            ReduceSeq(fun((acc, y) => multAndSumUp.apply(acc, Get(y, 0), Get(y, 1))), 0.0f) >>>
+            ReduceSeq(fun((acc, y) => multAndSumUp(acc, y._0, y._1)), 0.0f) >>>
             toLocal(MapSeq(id))
           )) :>>
           Join() :>>
@@ -89,7 +89,7 @@ object MatrixVector {
         )
       ) >>>
       Split(1) >>>
-      toGlobal(MapLcl(MapSeq(fun(x => multAndSumUp(Get(x, 0), Get(x, 1), beta))))) >>>
+      toGlobal(MapLcl(MapSeq(fun(x => multAndSumUp(x._0, x._1, beta))))) >>>
       Join()
     )
   })
@@ -111,47 +111,54 @@ object MatrixVector {
       ) $ Zip(matrix, vectorY)
     })
 
+  // The same expression as 'fullMatrixVectorFusedOpenCLAMD' but written in a
+  // dataflow / more imperative style
   val fullMatrixVectorFusedOpenCLAMD_ = fun(
-    ArrayType(ArrayType(Float, N), M),
-    ArrayType(Float, N),
-    ArrayType(ArrayType(Float, 1), M),
-    Float,
-    Float,
-    (matrix, vectorX, vectorY, alpha, beta) => {
-      Zip(matrix, vectorY) :>>
-      MapWrg(
-        \(t =>
-          Zip(
-            Zip(vectorX, Get(t, 0)) :>>
-            ReorderStride(128) :>>
-            Split(N/^128) :>>
-            toLocal(MapLcl(
-              ReduceSeq(\((acc, y) => multAndSumUp(acc, y._0, y._1)), 0.0f)) >>>
-              toLocal(MapSeq(id))
-            ) :>>
-            Join() :>>
-            Split(1) :>>
-            MapLcl(MapSeq(\(x => mult(alpha, x) ))) :>>
-            Join() :>>
-            Split(128) :>>
-            MapLcl(
-              ReduceSeq(add, 0.0f) >>>
-              toLocal(MapSeq(id))
-            ) :>>
-            Join()
-            ,
-            Get(t, 1)
-          )
-        ) >>>
-        Split(1) >>>
-        toGlobal(MapLcl(MapSeq(fun( x => multAndSumUp(x._0, x._1, beta))))) >>>
-        Join()
-      )
-    })
+     ArrayType(ArrayType(Float, N), M),
+     ArrayType(Float, N),
+     ArrayType(ArrayType(Float, 1), M),
+     Float,
+     Float,
+     (matrix, vectorX, vectorY, alpha, beta) => {
+       Zip(matrix, vectorY) :>>
+       MapWrg(
+         \(pair => {
+           val matrixRow = pair._0
+           val y_i       = pair._1
+
+           val partialDotProdcut = {
+             Zip(vectorX, matrixRow) :>>
+             ReorderStride(128) :>>
+             Split(N /^ 128) :>>
+             toLocal(MapLcl(
+               ReduceSeq(\((acc, y) => multAndSumUp(acc, y._0, y._1)), 0.0f) >>>
+               toLocal(MapSeq(id))
+             )) :>>
+             Join()
+           }
+
+           val timesAlpha = {
+             partialDotProdcut :>>
+             Split(1) :>> MapLcl(MapSeq(\(x => mult(alpha, x)))) :>> Join()
+           }
+
+           val fullDotProdcut = {
+             timesAlpha  :>>
+             Split(128) :>> MapLcl(ReduceSeq(add, 0.0f) >>> toLocal(MapSeq(id))) :>> Join()
+           }
+
+           Zip(fullDotProdcut, y_i)
+         }) >>>
+         Split(1) >>>
+         toGlobal(MapLcl(MapSeq(fun(x => multAndSumUp(x._0, x._1, beta))))) >>>
+         Join()
+       )
+     })
 
   def apply() = new MatrixVector(Seq(
     ("FULL_MATRIX_VECTOR_FUSED_OPENCL", Array[Lambda](fullMatrixVectorFusedOpenCL)),
-    ("FULL_MATRIX_VECTOR_FUSED_OPENCL_AMD", Array[Lambda](fullMatrixVectorFusedOpenCLAMD))))
+    ("FULL_MATRIX_VECTOR_FUSED_OPENCL_AMD", Array[Lambda](fullMatrixVectorFusedOpenCLAMD)),
+    ("FULL_MATRIX_VECTOR_FUSED_OPENCL_AMD_", Array[Lambda](fullMatrixVectorFusedOpenCLAMD_))))
 
   def main(args: Array[String]): Unit = {
     MatrixVector().run(args)
