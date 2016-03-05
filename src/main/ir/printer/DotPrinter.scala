de9d1a62d2cf7d9b2758f807537d952a558f7d1d
Christophe Dubach <christophe.dubach@ed.ac.uk>
2016-03-05 11:33:07 +0000

    - Improved Dot Printer - Added IRNode trait for Expr and Decl classes

--- src/main/ir/printer/DotPrinter.scala
+++ src/main/ir/printer/DotPrinter.scala
@@ -23,8 +23,120 @@ class DotPrinter(w: Writer) {
     "n"+Math.abs(n.hashCode())
   }
 
-  def print(decl: Lambda) = {
-    writeln("digraph{");
+  def print(node: IRNode) = {
+    writeln("digraph{")
+    node match {
+      case l: Lambda => countParams(l.body)
+      case e: Expr => countParams(e)
+    }
+    printNodes(node)
+    printEdges(node, "", "")
+    writeln("}")
+
+    w.flush()
+    w.close()
+  }
+
+
+  def printEdges(node : IRNode, parent: String, label: String, attr: String = "") : Unit = {
+
+    val nodeId = getNodeId(node)
+
+
+    node match {
+      case fc: FunCall =>
+        if (!parent.equals(""))
+          writeln (nodeId+" -> "+parent+" [label=\""+label+"\""+attr+"];")
+        fc.args.zipWithIndex.foreach(p=> printEdges(p._1, nodeId, "arg_"+p._2))
+        printEdges(fc.f, nodeId,"f")
+      case p : Param =>
+        if (!parent.equals(""))
+          writeln (nodeId+" -> "+parent+" [label=\""+label+"\""+attr+"];")
+      case l: Lambda =>
+        l.body match {
+          case fc: FunCall =>
+            if (fc.args.length == l.params.length)
+              if (fc.args.zip(l.params).map(p => p._1 == p._2 && counters.getOrElse(p._2,0) <= 2).fold(true)(_ && _)) {
+                printEdges(fc.f, parent, "o", ",color=Blue, style=dashed")
+                return
+              }
+        }
+        if (!parent.equals(""))
+          writeln (nodeId+" -> "+parent+" [label=\""+label+"\""+attr+"];")
+        l.params.zipWithIndex.foreach(p => printEdges(p._1, nodeId, "param_"+p._2))
+        printEdges(l.body, nodeId, "body")
+      case z: Zip =>
+        if (!parent.equals(""))
+          writeln (nodeId+" -> "+parent+" [label=\""+label+"\""+attr+"];")
+      case p: Pattern =>
+        if (!parent.equals(""))
+          writeln (nodeId+" -> "+parent+" [label=\""+label+"\""+attr+"];")
+        p match {
+          case fp: FPattern =>
+            printEdges(fp.f, nodeId, "f")
+          case j : Join =>
+          case s : Split =>
+        }
+      case uf: UserFun =>
+        if (!parent.equals(""))
+          writeln (nodeId+" -> "+parent+" [label=\""+label+"\""+attr+"];")
+    }
+  }
+
+  def printNodes(node: IRNode): Unit = {
+
+    if (visited.contains(node))
+      return
+    visited.add(node)
+
+    val nodeId = getNodeId(node)
+
+
+    node match {
+      case fc: FunCall =>
+        writeln(nodeId + " [style=rounded,shape=box,label=<<b>" + node.getClass.getSimpleName + "</b>>]")
+        fc.args.foreach(printNodes)
+        printNodes(fc.f)
+      case v: Value =>
+        writeln(nodeId + " [style=rounded,shape=box,label=<<b>" + node.getClass.getSimpleName + "</b>("+v.value+")>]")
+      case p: Param =>
+        writeln(nodeId + " [style=rounded,shape=box,label=<<b>" + node.getClass.getSimpleName + "</b>>]")
+
+      case l: Lambda =>
+
+        l.body match {
+          case fc: FunCall =>
+            if (fc.args.length == l.params.length)
+              if (fc.args.zip(l.params).map(p => p._1 == p._2 && counters.getOrElse(p._2,0) <= 2).fold(true)(_ && _)) {
+                printNodes(fc.f)
+                return
+              }
+        }
+
+        writeln(nodeId+" [style=rounded,shape=box,label=<<b>"+node.getClass.getSimpleName+"</b>>]")
+        l.params.foreach(p => printNodes(p))
+        printNodes(l.body)
+      case z: Zip =>
+        writeln(nodeId+" [style=rounded,shape=box,label=<<b>"+node.getClass.getSimpleName+"</b>>]")
+      case p: Pattern =>
+        p match {
+          case fp: FPattern =>
+            writeln(nodeId+" [style=rounded,shape=box,label=<<b>"+node.getClass.getSimpleName+"</b>>]")
+            printNodes(fp.f)
+          case j : Join =>
+            writeln(nodeId+" [style=rounded,shape=box,label=<<b>"+node.getClass.getSimpleName+"</b>>]")
+          case s : Split =>
+            writeln(nodeId+" [style=rounded,shape=box,label=<<b>"+node.getClass.getSimpleName+"</b>("+s.chunkSize+")>]")
+        }
+      case uf: UserFun =>
+        writeln(nodeId+" [style=rounded,shape=box,label=<<b>UserFun</b>("+uf.name+")>]")
+
+    }
+  }
+
+
+/*  def print(decl: Lambda) = {
+    writeln("digraph{")
     countParams(decl.body)
     printNodes(decl)
     printEdges(decl, "", "")
@@ -35,7 +147,7 @@ class DotPrinter(w: Writer) {
   }
 
   def print(expr: Expr) = {
-    writeln("digraph{");
+    writeln("digraph{")
     countParams(expr)
     printNodes(expr)
     printEdges(expr, "", "")
@@ -43,23 +155,24 @@ class DotPrinter(w: Writer) {
 
     w.flush()
     w.close()
-  }
+  }*/
 
   def countParams(expr: Expr) = {
     Expr.visit(expr,
-      pre => pre match {
-        case p : Param => counters.put(p,counters.getOrElse(p,0)+1)
+      {
+        case p: Param => counters.put(p, counters.getOrElse(p, 0) + 1)
         case _ =>
       }
       , post => {})
   }
+/*
 
-  def printEdges(expr : Expr, parent: String, label: String) : Unit = {
+  def printEdges(expr : Expr, parent: String, label: String, attr: String = "") : Unit = {
 
     val nodeId = getNodeId(expr)
 
     if (!parent.equals(""))
-      writeln (nodeId+" -> "+parent+" [label=\""+label+"\"];")
+      writeln (nodeId+" -> "+parent+" [label=\""+label+"\""+attr+"];")
 
     expr match {
       case fc: FunCall =>
@@ -69,30 +182,30 @@ class DotPrinter(w: Writer) {
     }
   }
 
-  def printEdges(decl : Decl, parent: String, label: String) : Unit = {
+  def printEdges(decl : Decl, parent: String, label: String, attr: String = "") : Unit = {
 
     val nodeId = getNodeId(decl)
 
-
-
     decl match {
       case l: Lambda =>
-
         l.body match {
           case fc: FunCall =>
             if (fc.args.length == l.params.length)
               if (fc.args.zip(l.params).map(p => p._1 == p._2 && counters.getOrElse(p._2,0) <= 2).fold(true)(_ && _)) {
-                printEdges(fc.f, parent, "f")
+                printEdges(fc.f, parent, "o", ",color=Blue, style=dashed")
                 return
               }
         }
-        if (!parent.equals("")) writeln (nodeId+" -> "+parent+" [label=\""+label+"\"];")
+        if (!parent.equals(""))
+          writeln (nodeId+" -> "+parent+" [label=\""+label+"\""+attr+"];")
         l.params.zipWithIndex.foreach(p => printEdges(p._1, nodeId, "param_"+p._2))
         printEdges(l.body, nodeId, "body")
       case z: Zip =>
-        if (!parent.equals("")) writeln (nodeId+" -> "+parent+" [label=\""+label+"\"];")
+        if (!parent.equals(""))
+          writeln (nodeId+" -> "+parent+" [label=\""+label+"\""+attr+"];")
       case p: Pattern =>
-        if (!parent.equals("")) writeln (nodeId+" -> "+parent+" [label=\""+label+"\"];")
+        if (!parent.equals(""))
+          writeln (nodeId+" -> "+parent+" [label=\""+label+"\""+attr+"];")
         p match {
           case fp: FPattern =>
             printEdges(fp.f, nodeId, "f")
@@ -100,26 +213,29 @@ class DotPrinter(w: Writer) {
           case s : Split =>
         }
       case uf: UserFun =>
-        if (!parent.equals("")) writeln (nodeId+" -> "+parent+" [label=\""+label+"\"];")
+        if (!parent.equals(""))
+          writeln (nodeId+" -> "+parent+" [label=\""+label+"\""+attr+"];")
     }
   }
 
-    def printNodes(expr : Expr) : Unit = {
+  def printNodes(expr: Expr): Unit = {
 
-      if (visited.contains(expr))
-        return
-      visited.add(expr)
+    if (visited.contains(expr))
+      return
+    visited.add(expr)
 
     val nodeId = getNodeId(expr)
 
 
-      writeln(nodeId+" [style=rounded,shape=box,label=<<b>"+expr.getClass.getSimpleName+"</b>>]")
-
     expr match {
       case fc: FunCall =>
+        writeln(nodeId + " [style=rounded,shape=box,label=<<b>" + expr.getClass.getSimpleName + "</b>>]")
         fc.args.foreach(printNodes)
         printNodes(fc.f)
-      case p : Param =>
+      case v: Value =>
+        writeln(nodeId + " [style=rounded,shape=box,label=<<b>" + expr.getClass.getSimpleName + "</b>("+v.value+")>]")
+      case p: Param =>
+        writeln(nodeId + " [style=rounded,shape=box,label=<<b>" + expr.getClass.getSimpleName + "</b>>]")
     }
   }
 
@@ -163,44 +279,6 @@ class DotPrinter(w: Writer) {
 
   }
   }
+*/
 
 }
-/*
-
-
-\node               (applyRS) {\textbf{apply}};
-  \node               (join)  [below of=applyRS]         {\textbf{join}};
-  \node               (reduceSeq)  [right of=applyRS, node distance=2cm]   {\textbf{reduceSeq}};
-  \node[draw=none] (dummy1) [below of=reduceSeq] {};
-  \node               (LRS) [below of=reduceSeq] {$\lambda$};
-  \node               (inputLRS) [below of=LRS] {input};
-  \node               (applyLRS) [right of=inputLRS] {apply};
-  \node[draw=none] (dummy2) [below of=applyLRS] {};
-  \node               (add)  [right of=applyLRS]         {$+$};
-
-  \node               (applyMW) [below of=join] {\textbf{apply}};
-  \node               (split128)  [below of=applyMW]         {$\textbf{split}^{128}$};
-  \node               (mapWrg)  [right of=split128]   {\textbf{mapWrg}};
-  \node               (zip) [below of=split128]             {\textbf{zip}};
-  \node               (input1)  [below left of=zip]                      {input};
-  \node               (input2)  [below right of=zip]         {input};
-  \node[fill=blue!10]               (dotProdInWrg)  [right of=zip, node distance=2cm]   {dotProdInWrg};
-
-  \begin{scope}[every node/.style={scale=.75}]
-  \path[->]
-  (input1)     edge                  node[left]           {in1}         (zip)
-  (input2)     edge                  node[right]           {in2}         (zip)
-  (zip)        edge                  node           {in}          (split128)
-  (split128)   edge                  node           {in}          (applyMW)
-  (applyMW)    edge                  node           {in}          (join)
-  (mapWrg)     edge                  node[right]           {f}          (applyMW)
-  (join)       edge                  node           {in}          (applyRS)
-  (reduceSeq)    edge                  node            {f}          (applyRS)
-  (add)        edge                  node           {f}           (applyLRS)
-  (dotProdInWrg) edge                node[right]           {f}           (mapWrg)
-  (LRS) edge node {l} (reduceSeq)
-  (inputLRS) edge node {p} (LRS)
-  (inputLRS) edge [bend right=90] node {in} (applyLRS)
-  %(inputLRS) edge [style={-}] node {p} (LRS)
-  (applyLRS) edge node {in} (LRS);
-  \end{scope}*/
