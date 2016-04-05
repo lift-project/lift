2ebda0fa5af1758f01cab0a1048544bd1f95c5ab
Bastian Hagedorn <b_hage09@wwu.de>
2016-04-05 17:47:51 +0100

    added testcases for group primitive

--- src/test/opencl/generator/TestGroup.scala
+++ src/test/opencl/generator/TestGroup.scala
@@ -2,13 +2,12 @@ package opencl.generator
 
 import apart.arithmetic.Var
 import ir.ArrayType
-import ir.ast.{Pad, Zip, Group}
+import ir.ast.{Group}
 import opencl.executor.{Execute, Executor}
 import opencl.ir._
-import opencl.ir.pattern.{MapGlb, MapSeq, ReduceSeq, toGlobal}
+import opencl.ir.pattern.{MapGlb, MapSeq}
 import org.junit.Assert._
 import org.junit.{AfterClass, BeforeClass, Test}
-import spl.Stencil
 import ir.ast.fun
 
 
@@ -29,27 +28,71 @@ object TestGroup {
   * Created by Bastian Hagedorn on 04.04.16.
   */
 class TestGroup {
-  // TODO Transform Group primitive to accept negative relative indices.
-  @Test def GROUP_1D(): Unit = {
+  @Test def groupLeftCurrentRight(): Unit = {
     val data = Array.tabulate(5)(_*1.0f)
     val gold = Array(0.0f, 1.0f, 2.0f, 1.0f, 2.0f, 3.0f, 2.0f, 3.0f, 4.0f)
-    val indices = Array(-1 , 0 , 1)
-    //val indices = Array(1,2)
-
+    val indices = Array(-1, 0 , 1)
 
     val groupFun = fun(
       ArrayType(Float, Var("N")),
       (domain) => MapGlb(MapSeq(id)) o Group(indices) $ domain
     )
 
-    //val (output: Array[Float], runtime) = Execute(data.length)(groupFun, data, weights)
     val (output: Array[Float],runtime) = Execute(data.length, 10)(groupFun, data)
-    println("output.length = " + output.length)
-    println("output(0) = " + output(0))
+
     println("runtime = " + runtime)
-
     println(output.mkString(", "))
-
     assertArrayEquals(gold, output, 0.00001f)
+  }
+
+  // TODO (@Bastian) fix me
+  @Test def groupCurrentWithTwoRightNeighbours(): Unit = {
+    val data = Array.tabulate(5)(_*1.0f)
+    val gold = Array(0.0f, 1.0f, 2.0f, 1.0f, 2.0f, 3.0f, 2.0f, 3.0f, 4.0f)
+    val indices = Array(0, 1, 2)
+
+    val groupFun = fun(
+      ArrayType(Float, Var("N")),
+      (domain) => MapGlb(MapSeq(id)) o Group(indices) $ domain
+    )
+
+    val (output: Array[Float],runtime) = Execute(data.length, 10)(groupFun, data)
+
+    println("runtime = " + runtime)
+    println(output.mkString(", "))
+    assertArrayEquals(gold, output, 0.00001f)
+  }
+
+  @Test def groupCurrentWithTwoLeftNeighbours(): Unit = {
+    val data = Array.tabulate(5)(_*1.0f)
+    val gold = Array(0.0f, 1.0f, 2.0f, 1.0f, 2.0f, 3.0f, 2.0f, 3.0f, 4.0f)
+    val indices = Array(-2, -1 ,0)
+
+    val groupFun = fun(
+      ArrayType(Float, Var("N")),
+      (domain) => MapGlb(MapSeq(id)) o Group(indices) $ domain
+    )
+
+    val (output: Array[Float],runtime) = Execute(data.length, 10)(groupFun, data)
+
+    println("runtime = " + runtime)
+    println(output.mkString(", "))
+    assertArrayEquals(gold, output, 0.00001f)
+  }
+
+  @Test def groupIdentity(): Unit = {
+    val data = Array.tabulate(5)(_*1.0f)
+    val indices = Array(0)
+
+    val groupFun = fun(
+      ArrayType(Float, Var("N")),
+      (domain) => MapGlb(MapSeq(id)) o Group(indices) $ domain
+    )
+
+    val (output: Array[Float],runtime) = Execute(data.length, 10)(groupFun, data)
+
+    println("runtime = " + runtime)
+    println(output.mkString(", "))
+    assertArrayEquals(data, output, 0.00001f)
   }
 }
