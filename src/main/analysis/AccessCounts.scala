5a9560aa0c528e8d1a796551512ec37d1ebb9ede
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2016-04-26 15:52:05 +0100

    Adding the option to get exact counts

--- src/main/analysis/AccessCounts.scala
+++ src/main/analysis/AccessCounts.scala
@@ -1,20 +1,50 @@
 package analysis
 
+import analysis.AccessCounts.SubstitutionMap
+import apart.arithmetic.ArithExpr.{contains, substitute}
 import apart.arithmetic._
 import ir._
 import ir.ast._
-import opencl.executor.Compile
-import opencl.ir.{OpenCLAddressSpace, OpenCLMemory}
+import opencl.generator.OpenCLGenerator.NDRange
+import opencl.generator._
+import opencl.ir._
 import opencl.ir.pattern._
+import rewriting.InferNDRange.substituteInNDRange
 
 object AccessCounts {
 
-  def apply(lambda: Lambda) =
-    new AccessCounts(lambda)
+  type SubstitutionMap = collection.immutable.Map[ArithExpr, ArithExpr]
+
+  def apply(
+    lambda: Lambda,
+    localSize: NDRange = Array(?,?,?),
+    globalSize: NDRange = Array(?,?,?),
+    valueMap: SubstitutionMap = collection.immutable.Map()
+  ) = new AccessCounts(lambda, localSize, globalSize, valueMap)
 
 }
 
-class AccessCounts(val lambda: Lambda) {
+class AccessCounts(
+  val lambda: Lambda,
+  val localSize: NDRange,
+  val globalSize: NDRange,
+  val valueMap: SubstitutionMap
+) {
+
+  private val substLocal = substituteInNDRange(localSize, valueMap)
+  private val substGlobal = substituteInNDRange(globalSize, valueMap)
+
+  private val substitutionMap = collection.immutable.Map[ArithExpr, ArithExpr](
+    new get_local_size(0) -> substLocal(0),
+    new get_local_size(1) -> substLocal(1),
+    new get_local_size(2) -> substLocal(2),
+    new get_global_size(0) -> substGlobal(0),
+    new get_global_size(1) -> substGlobal(1),
+    new get_global_size(2) -> substGlobal(2),
+    new get_num_groups(0) -> (substGlobal(0) / substLocal(0)),
+    new get_num_groups(1) -> (substGlobal(1) / substLocal(1)),
+    new get_num_groups(2) -> (substGlobal(2) / substLocal(2))
+  ).filterNot(pair => contains(pair._2, ?)) ++ valueMap
 
   private var loads = collection.Map[Memory, ArithExpr]()
   private var stores = collection.Map[Memory, ArithExpr]()
@@ -31,31 +61,51 @@ class AccessCounts(val lambda: Lambda) {
       .groupBy({ case (mem: OpenCLMemory, _) => mem.addressSpace })
       .map(kv => (kv._1, kv._2.foldLeft(Cst(0): ArithExpr)((acc, curr) => acc + curr._2)))
 
-  // TODO: only do the required steps and only if not already done
-  Compile(lambda)
+  if (lambda.body.t == UndefType)
+    TypeChecker(lambda)
+
+  if (lambda.body.mem == UnallocatedMemory) {
+    RangesAndCounts(lambda, localSize, globalSize, valueMap)
+    OpenCLMemoryAllocator(lambda)
+  }
+
   count(lambda.body)
+
+  private def getExact(arithExpr: ArithExpr, exact: Boolean) =
+    if (exact) substitute(arithExpr, substitutionMap) else arithExpr
+
+  override def toString: String = {
+    val exact = true
+    s"""Stores to global: ${getExact(storesToAddressSpace(GlobalMemory), exact)}
+    |Loads from global: ${getExact(loadsToAddressSpace(GlobalMemory), exact)}
+    |Stores to local: ${getExact(storesToAddressSpace(LocalMemory), exact)}
+    |Loads from local: ${getExact(loadsToAddressSpace(LocalMemory), exact)}
+    |Stores to private: ${getExact(storesToAddressSpace(PrivateMemory), exact)}
+    |Loads form private: ${getExact(loadsToAddressSpace(PrivateMemory), exact)}""".stripMargin
+  }
 
   def accesses = (loads, stores)
 
   // TODO: # accesses with pattern
   // TODO: # vector accesses
 
-  def loadsToMemory(memory: Memory) = loads.getOrElse(memory, Cst(0))
+  def loadsToMemory(memory: Memory, exact: Boolean = false) =
+    getExact(loads.getOrElse(memory, Cst(0)), exact)
 
-  def storesToMemory(memory: Memory) = stores.getOrElse(memory, Cst(0))
+  def storesToMemory(memory: Memory, exact: Boolean = false) =
+    getExact(stores.getOrElse(memory, Cst(0)), exact)
 
-  def accessesToMemory(memory: Memory) =
-    loadsToMemory(memory) + storesToMemory(memory)
+  def accessesToMemory(memory: Memory, exact: Boolean = false) =
+    loadsToMemory(memory, exact) + storesToMemory(memory, exact)
 
+  def loadsToAddressSpace(addressSpace: OpenCLAddressSpace, exact: Boolean = false) =
+    getExact(loadsToAllAddressSpaces.getOrElse(addressSpace, Cst(0)), exact)
 
-  def loadsToAddressSpace(addressSpace: OpenCLAddressSpace) =
-    loadsToAllAddressSpaces.getOrElse(addressSpace, Cst(0))
+  def storesToAddressSpace(addressSpace: OpenCLAddressSpace, exact: Boolean = false) =
+    getExact(storesToAllAddressSpaces.getOrElse(addressSpace, Cst(0)), exact)
 
-  def storesToAddressSpace(addressSpace: OpenCLAddressSpace) =
-    storesToAllAddressSpaces.getOrElse(addressSpace, Cst(0))
-
-  def accessesToAddressSpace(addressSpace: OpenCLAddressSpace) =
-    loadsToAddressSpace(addressSpace) + storesToAddressSpace(addressSpace)
+  def accessesToAddressSpace(addressSpace: OpenCLAddressSpace, exact: Boolean = false) =
+    loadsToAddressSpace(addressSpace, exact) + storesToAddressSpace(addressSpace, exact)
 
   private def count(lambda: Lambda, arithExpr: ArithExpr): Unit = {
     currentNesting *= arithExpr
