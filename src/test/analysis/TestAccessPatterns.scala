498658787e84b3ca0a1ab9038298569ad44fb649
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2016-06-21 14:52:37 +0100

    Detecting coalesced vector accesses

--- src/test/analysis/TestAccessPatterns.scala
+++ src/test/analysis/TestAccessPatterns.scala
@@ -14,6 +14,7 @@ class TestAccessPatterns {
   private def findRead(lambda: Lambda) = {
     Expr.visitWithState(None: Option[Expr])(lambda.body, {
       case (FunCall(_:UserFun, arg), _) => Some(arg)
+      case (FunCall(_:VectorizeUserFun, arg), _) => Some(arg)
       case (_, read) => read
     }).get
   }
@@ -130,6 +131,24 @@ class TestAccessPatterns {
   def strideGlb(): Unit = {
     val f = fun(ArrayType(Float, N),
       x => MapGlb(0)(id) o Gather(ReorderWithStride(16)) $ x
+    )
+
+    assertFalse(isReadCoalesced(f))
+  }
+
+  @Test
+  def simpleVectorised(): Unit = {
+    val f = \(ArrayType(Float, N),
+      MapGlb(VectorizeUserFun(4, id)) o asVector(4) $ _
+    )
+
+    assertTrue(isReadCoalesced(f))
+  }
+
+  @Test
+  def stridedVectorised(): Unit = {
+    val f = \(ArrayType(Float, N),
+      MapGlb(VectorizeUserFun(4, id)) o Gather(ReorderWithStride(32)) o asVector(4) $ _
     )
 
     assertFalse(isReadCoalesced(f))
