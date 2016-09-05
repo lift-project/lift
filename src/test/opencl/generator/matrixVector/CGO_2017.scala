6bff650929bd1ff7473fbc2034a8c7056c8d86a0
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2016-09-05 14:57:54 +0100

    Add clblast gemv T kernel

--- src/test/opencl/generator/matrixVector/CGO_2017.scala
+++ src/test/opencl/generator/matrixVector/CGO_2017.scala
@@ -74,17 +74,7 @@ class CGO_2017 {
   }
 
   @Test
-  def clblast_gemv_kepler_N(): Unit = {
-
-  }
-
-  @Test
-  def clblast_gemv_kepler_T(): Unit = {
-
-  }
-
-  @Test
-  def clblast_gemv_hawaii_N(): Unit = {
+  def clblast_gemv_N(): Unit = {
 
     val f = fun(
       ArrayType(ArrayType(Float, N), M),
@@ -124,7 +114,43 @@ class CGO_2017 {
   }
 
   @Test
-  def clblast_gemv_hawaii_T(): Unit = {
+  def clblast_gemv_T(): Unit = {
+
+    val f = fun(
+      ArrayType(ArrayType(Float, M), N),
+      ArrayType(Float, N),
+      ArrayType(Float,M),
+      Float,
+      Float,
+      (matrix, vectorX, vectorY, alpha, beta) =>
+        Join() o MapWrg(fun( matChunk =>
+
+          MapSeq(
+            toGlobal(fun(y =>
+              MapLcl(fun(x =>
+                add(
+                  toPrivate(mult)(x._0, alpha),
+                  toPrivate(mult)(x._1, beta)
+                )
+              )) $ Zip(y, Map(Get(1)) $ matChunk)
+            ))
+          ) o
+            ReduceSeq(fun((acc, next) =>
+              Let(localX =>
+                Join() o MapLcl(fun(x => ReduceSeq(fun((acc2, next2) =>
+                  multAndSumUp(acc2, Get(next2, 0), Get(next2, 1)))
+                  , Get(x, 0)) $ Zip(Get(x, 1), localX))) $ Zip(acc, Get(next, 0))
+              )  o toLocal(MapLcl(id)) $ Get(next, 1)),
+
+              MapLcl(id) $ Value(0.0f, ArrayType(Float, 64)))
+            $ Zip(Transpose() o Map(Split(64) o Get(0)) $ matChunk, Split(64) $ vectorX)
+        )) o Split(64) $ Zip(Transpose() $ matrix, vectorY)
+    )
+
+    val (result: Array[Float], _) =
+      Execute(64, n, (true, true))(f, matrix.transpose, vectorX, vectorY, alpha, beta)
+
+    assertArrayEquals(gold, result, 0.001f)
 
   }
 }
