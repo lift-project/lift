67f3a53332a1304297e2a09c34c8b8a9125c0c79
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2016-11-30 13:39:47 +0000

    Add more tests for issue #76. Mistake caused by the private memory access

--- src/test/opencl/generator/TestMisc.scala
+++ src/test/opencl/generator/TestMisc.scala
@@ -232,6 +232,59 @@ class TestMisc {
     assertArrayEquals(gold, result, 0.0f)
   }
 
+  @Test
+  def issue76_2(): Unit = {
+    val f = \(
+      Float,
+      ArrayType(Float, 32),
+      (const, arr) =>
+        MapGlb(\(a => add(toGlobal(id) $ const, add(const, a)))) $ arr
+    )
+
+    val input = Array.tabulate(32)(_.toFloat)
+    val const = 2.0f
+
+    val gold = input.map(_ + 2*const)
+
+    val (result: Array[Float], _) = Execute(1,1)(f, const, input)
+
+    assertArrayEquals(gold, result, 0.0f)
+  }
+
+  @Test
+  def issue76_3(): Unit = {
+    val f = \(
+      ArrayType(Float, 32),
+      ArrayType(Float, 32),
+      (arr1, arr2) =>
+        MapGlb(\(a => MapSeq(\(b => add(toPrivate(id) $ b, add(a, b)))) $ arr2)) $ arr1
+    )
+
+    val input = Array.tabulate(32)(_.toFloat)
+
+    val gold = input.flatMap(a => input.map(b => a + a + b))
+
+    val (result: Array[Float], _) = Execute(1,1)(f, input, input)
+    assertArrayEquals(gold, result, 0.0f)
+  }
+
+  @Test
+  def issue76_4(): Unit = {
+    val f = \(
+      ArrayType(Float, 32),
+      ArrayType(Float, 32),
+      (arr1, arr2) =>
+        MapGlb(\(a => MapSeq(\(b => add(add(a, b), toPrivate(id) $ b))) $ arr2)) $ arr1
+    )
+
+    val input = Array.tabulate(32)(_.toFloat)
+
+    val gold = input.flatMap(a => input.map(b => a + a + b))
+
+    val (result: Array[Float], _) = Execute(1,1)(f, input, input)
+    assertArrayEquals(gold, result, 0.0f)
+  }
+
   @Test def issue22(): Unit = {
     val inputSize = 1024
     val inputData = Array.fill(inputSize)(util.Random.nextInt(5).toFloat)
