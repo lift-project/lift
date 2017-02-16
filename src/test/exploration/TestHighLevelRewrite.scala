29bd1e1fde379fced8abd3de495751d841c40f77
Bastian Hagedorn <b.hagedorn@wwu.de>
2017-02-16 15:12:40 +0100

    fixed value for stencil test

--- src/test/exploration/TestHighLevelRewrite.scala
+++ src/test/exploration/TestHighLevelRewrite.scala
@@ -82,7 +82,7 @@ class TestHighLevelRewrite {
   def stencil1DRewrite(): Unit = {
 
     val rewriter = new HighLevelRewrite(4, 2)
-    val rewrittenLambdas = rewriter(stencil1D, 5)
+    val rewrittenLambdas = rewriter(stencil1D, 2)
 
     val gold = fun(ArrayType(Float, N),(p_0) => FunCall(Join(), FunCall(Join(), FunCall(Map(fun((p_1) => FunCall(Map(fun((p_2) => FunCall(Reduce(fun((p_3, p_4) => FunCall(add, p_3, p_4))), Value("0.0f", Float), p_2))), FunCall(Slide(3,1), p_1)))), FunCall(Slide((2+v__1),v__1), FunCall(Pad(1,1,Pad.Boundary.Clamp), p_0))))))
     val goldHash = getHash(gold)
