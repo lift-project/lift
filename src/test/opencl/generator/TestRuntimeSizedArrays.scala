c12487ebbb10e4c1074a0167f920762bc9fffffc
Michel Steuwer <michel.steuwer@ed.ac.uk>
2017-03-21 16:55:56 +0000

    Added (failing) nested RuntimeSizedArray test

--- src/test/opencl/generator/TestRuntimeSizedArrays.scala
+++ src/test/opencl/generator/TestRuntimeSizedArrays.scala
@@ -1,12 +1,13 @@
 package opencl.generator
 
 import ir.ast.fun
-import ir.RuntimeSizedArrayType
+import ir.{ArrayType, RuntimeSizedArrayType}
+import lift.arithmetic.SizeVar
 import opencl.executor.{Execute, Executor}
 import opencl.ir._
 import opencl.ir.pattern._
 import org.junit.Assert._
-import org.junit.{AfterClass, BeforeClass, Test}
+import org.junit.{AfterClass, BeforeClass, Ignore, Test}
 
 object TestRuntimeSizedArrays {
   @BeforeClass def before(): Unit = {
@@ -55,6 +56,32 @@ class TestRuntimeSizedArrays {
     val (output: Array[Float], runtime) = Execute(inputData.length)(l, inputData)
 
     assertEquals(inputData.drop(1).sum, output.sum, 0.0)
+
+    println("output(0) = " + output(0))
+    println("runtime = " + runtime)
+  }
+
+  @Ignore
+  @Test
+  def nestedReduceFloat(): Unit = {
+    def createRow(inputSize: Int) = {
+      Array(java.lang.Float.intBitsToFloat(inputSize)) ++
+        Array.fill(inputSize)(util.Random.nextInt(5).toFloat)
+    }
+
+    val size = 128
+    val inputData = Array.tabulate(size)(i => createRow(100 + i))
+
+    val N = SizeVar("N")
+
+    val l = fun(
+      ArrayType(RuntimeSizedArrayType(Float), N),
+      a => MapGlb(toGlobal(MapSeq(id)) o ReduceSeq(fun((acc, x) => add(acc, x)), 0.0f)) $ a
+    )
+
+    val (output: Array[Float], runtime) = Execute(inputData.length)(l, inputData)
+
+    assertEquals(inputData.map(_.drop(1).sum).sum, output.sum, 0.0)
 
     println("output(0) = " + output(0))
     println("runtime = " + runtime)
