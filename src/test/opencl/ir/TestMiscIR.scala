2a51ce72c2f55c614681e0f3155f666a80998187
federico <pizzutifederico27@gmail.com>
2017-07-25 10:53:22 +0100

    Added a test ensuring one can build a tuple Value from lift Expr(s) (as opposed to just using scala types coerced into Lift's equivalent type)

---
+++ src/test/opencl/ir/TestMiscIR.scala
@@ -0,0 +1,19 @@
+package opencl.ir
+
+import ir.TypeChecker
+import ir.ast.{Expr, Value}
+import org.junit.Test
+
+/**
+  * Created by federico on 25/07/17.
+  */
+
+class TestMiscIR {
+  @Test
+  def buildTupleFromExprTest():Unit = {
+    val expr1:Expr = 1.0f
+    val expr2:Expr = 0.0f
+    val tuple:Value = (expr1, expr2)
+    TypeChecker.check(tuple)
+  }
+}
