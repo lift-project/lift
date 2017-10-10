d5a1ddf2211eada369f2010c09e66099a714ad7d
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2017-10-10 14:08:13 +0100

    Move the communication one to a separate file

---
+++ src/test/exploration/DetectCommunication.scala
@@ -0,0 +1,56 @@
+package exploration
+
+import ir._
+import ir.ast._
+import lift.arithmetic._
+import opencl.ir._
+import opencl.ir.pattern._
+import org.junit.Test
+
+class DetectCommunication {
+
+  private val v_M_0 = Var("M", StartFromRange(1))
+  private val v_N_1 = Var("N", StartFromRange(1))
+
+  @Test
+  def partialReduceWithReorderNoRace(): Unit = {
+
+    // Communication between threads
+    val f = fun(
+      ArrayType(ArrayType(Float, v_M_0), v_N_1),
+      ArrayType(Float, v_M_0),
+      ArrayType(Float, v_N_1),
+      Float, Float,
+      (p_0, p_1, p_2, p_3, p_4) =>
+        FunCall(MapWrg(0)(fun((p_5) =>
+          FunCall(Join(),
+            FunCall(MapLcl(0)(fun((p_6) =>
+              FunCall(toGlobal(fun((p_7) =>
+                FunCall(MapSeq(fun((p_8) =>
+                  FunCall(add,
+                    FunCall(mult, p_8, p_3),
+                    FunCall(mult,
+                      FunCall(Get(1), p_5), p_4)))), p_7))),
+                FunCall(ReduceSeq(fun((p_9, p_10) =>
+                  FunCall(add, p_9, p_10))),
+                  FunCall(idfloat, Value("0.0f", Float)), p_6)))),
+              FunCall(Split(Cst(128)),
+                FunCall(Join(),
+                  FunCall(MapLcl(0)(fun((p_11) =>
+                    FunCall(ReduceSeq(fun((p_12, p_13) =>
+                      FunCall(add, p_12,
+                        FunCall(mult,
+                          FunCall(Get(0), p_13),
+                          FunCall(Get(1), p_13))))),
+                      FunCall(idfloat, Value("0.0f", Float)), p_11))),
+                    FunCall(Split( v_M_0 * Pow(Cst(128), Cst(-1)) ),
+                      FunCall(Gather(ReorderWithStride(128)),
+                        FunCall(Zip(2), p_1,
+                          FunCall(Get(0), p_5))))))))))),
+          FunCall(Zip(2), p_0, p_2)))
+
+    // addIdAfterReduce / toLocal for last write
+    println(f)
+  }
+
+}
--- src/test/exploration/DetectReuseAcrossThreads.scala
+++ src/test/exploration/DetectReuseAcrossThreads.scala
@@ -223,46 +223,6 @@ class DetectReuseAcrossThreads {
     printStrategicLocalMemoryLocations(f)
   }
 
-  @Test
-  def partialReduceWithReorderNoRace(): Unit = {
-
-    // Communication between threads
-    val f = fun(
-      ArrayType(ArrayType(Float, v_M_0), v_N_1),
-      ArrayType(Float, v_M_0),
-      ArrayType(Float, v_N_1),
-      Float, Float,
-      (p_0, p_1, p_2, p_3, p_4) =>
-        FunCall(MapWrg(0)(fun((p_5) =>
-          FunCall(Join(),
-            FunCall(MapLcl(0)(fun((p_6) =>
-              FunCall(toGlobal(fun((p_7) =>
-                FunCall(MapSeq(fun((p_8) =>
-                  FunCall(add,
-                    FunCall(mult, p_8, p_3),
-                    FunCall(mult,
-                      FunCall(Get(1), p_5), p_4)))), p_7))),
-                FunCall(ReduceSeq(fun((p_9, p_10) =>
-                  FunCall(add, p_9, p_10))),
-                  FunCall(idfloat, Value("0.0f", Float)), p_6)))),
-              FunCall(Split(Cst(128)),
-                FunCall(Join(),
-                  FunCall(MapLcl(0)(fun((p_11) =>
-                    FunCall(ReduceSeq(fun((p_12, p_13) =>
-                      FunCall(add, p_12,
-                        FunCall(mult,
-                          FunCall(Get(0), p_13),
-                          FunCall(Get(1), p_13))))),
-                      FunCall(idfloat, Value("0.0f", Float)), p_11))),
-                    FunCall(Split( v_M_0 * Pow(Cst(128), Cst(-1)) ),
-                      FunCall(Gather(ReorderWithStride(128)),
-                        FunCall(Zip(2), p_1,
-                          FunCall(Get(0), p_5))))))))))),
-          FunCall(Zip(2), p_0, p_2)))
-
-
-    printStrategicLocalMemoryLocations(f)
-  }
 
   @Test
   def mmTATiled2DBlocked(): Unit = {
