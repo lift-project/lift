bce8ecf9cf0ee00fa84547b2f49ca54b9803bccc
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2017-10-10 10:30:23 +0100

    Forgot a file

---
+++ src/test/exploration/DetectReuseWithinThread.scala
@@ -0,0 +1,232 @@
+package exploration
+
+import ir._
+import ir.ast._
+import ir.view.{View, ViewAccess, ViewMap, ViewMem}
+import lift.arithmetic.{?, RangeMul, StartFromRange, Var}
+import opencl.generator.{NDRange, RangesAndCounts}
+import opencl.ir._
+import opencl.ir.pattern._
+import org.junit.Test
+
+class DetectReuseWithinThread {
+
+  private def getNumDimensions(lambda: Lambda): Int = {
+
+    val dims = Expr.visitWithState(Set[Int]())(lambda.body, {
+      case (FunCall(MapLcl(dim, _), _), set) => set + dim
+      case (FunCall(MapGlb(dim, _), _), set) => set + dim
+      case (_, set) => set
+    })
+
+    dims.size
+  }
+
+  private def getReuseCandidates(f: Lambda) = {
+    val numDimensions = getNumDimensions(f)
+
+    TypeChecker(f)
+    InferOpenCLAddressSpace(f)
+    RangesAndCounts(f, NDRange(?, ?, ?), NDRange(?, ?, ?), collection.Map())
+    OpenCLMemoryAllocator(f)
+    View(f)
+    UpdateContext(f)
+
+    val args = Expr.visitWithState(Seq[Expr]())(f.body, {
+      case (call@FunCall(_: UserFun | _: VectorizeUserFun, args@_*), seq)
+        if call.context.inMapLcl.count(b => b) + call.context.inMapGlb.count(b => b) == numDimensions
+      => seq ++ args
+      case (_, seq) => seq
+    }).distinct.diff(f.params).filter({
+      case FunCall(_: UserFun | _: VectorizeUserFun, _*) => false
+      case _: Value => false
+      case _ => true
+    })
+
+    args.filterNot(arg => getNumberOfPrivateAccesses(arg) >= getNumberOfSequentialDimensions(f, arg))
+  }
+
+  private def getNumberOfSequentialDimensions(f: Lambda, expr: Expr) = {
+    Expr.visitWithState(0)(f.body, {
+      case (FunCall(fp: FPattern, _*), count) if fp.isInstanceOf[MapSeq] || fp.isInstanceOf[ReduceSeq] =>
+        // e is sequential
+        // e contains expr
+        // e doesn't contain parallel
+
+        if (
+          fp.f.body.contains({ case e if e eq expr => }) &&
+            !fp.f.body.contains({ case FunCall(MapLcl(_, _), _) => }) &&
+            !fp.f.body.contains({ case FunCall(MapWrg(_, _), _) => }) &&
+            !fp.f.body.contains({ case FunCall(MapGlb(_, _), _) => })
+        )
+          count + 1
+        else
+          count
+      case (_, count) => count
+    })
+  }
+
+  private def getNumberOfPrivateAccesses(expr: Expr) = {
+    val views = View.getSubViews(expr.view)
+
+    val sequentialMapViews = views.takeWhile({
+      case ViewAccess(v, _, _) => v.toString.startsWith("v_i_")
+      case ViewMap(_, v, _) => v.toString.startsWith("v_i_")
+      case _ => true
+    })
+
+    val viewMaps = sequentialMapViews.count({
+      case ViewMap(_, v, _) => v.toString.startsWith("v_i_")
+      case _ => false
+    })
+
+    val viewAccesses = sequentialMapViews.count({
+      case ViewAccess(v, _, _) => v.toString.startsWith("v_i_")
+      case _ => false
+    })
+
+    viewAccesses - viewMaps
+  }
+
+  private def printStrategicLocations(lambda: Lambda): Unit = {
+    val strategicLocationsMarked = MemoryMappingRewrite.addIdsForPrivate(lambda)
+    val reuseCandidates = getReuseCandidates(strategicLocationsMarked)
+    val tryHere = reuseCandidates.flatMap(getRuleLocationCandidates(strategicLocationsMarked, _))
+
+    println
+    println(strategicLocationsMarked)
+    println(tryHere.mkString(", "))
+    println
+  }
+
+  private def getRuleLocationCandidates(strategicLocationsMarked: Lambda, reuseExpr: Expr) = {
+    val sourceView = View.getSubViews(reuseExpr.view).last
+
+    sourceView match {
+      case ViewMem(sourceVar, _) =>
+
+        // Find which "strategic" Id location(s) would copy the required variable and is suitable for local memory
+        // TODO: Doesn't work for all reuse... Does it matter? Still gets what we care about
+        Expr.visitWithState(Seq[(Expr, Var)]())(strategicLocationsMarked.body, {
+          case (funCall@FunCall(Id(), _*), seq)
+            if getAllMemoryVars(funCall.mem).contains(sourceVar) &&
+              (funCall.context.inMapLcl.reduce(_ || _) || funCall.context.inMapGlb.reduce(_ || _)) // TODO: MapGlb + insideAll
+          =>
+            seq :+ (funCall, sourceVar)
+          case (_, seq) => seq
+        })
+
+      case _ => Seq()
+    }
+  }
+
+  private def getAllMemoryVars(memory: Memory): Seq[Var] = {
+    memory match {
+      case OpenCLMemoryCollection(subMemories, _) => subMemories.flatMap(getAllMemoryVars)
+      case _ => Seq(memory.variable)
+    }
+  }
+
+  @Test
+  def oneDRegBlock(): Unit = {
+    val v_K_0 = Var("K", StartFromRange(1))
+    val v_M_1 = Var("M", StartFromRange(1))
+    val v_N_2 = Var("N", StartFromRange(1))
+    val v__3 = Var("", RangeMul(1,1+v_M_1,2))
+
+    val f = fun(
+      ArrayType(ArrayType(Float, v_K_0), v_M_1),
+      ArrayType(ArrayType(Float, v_K_0), v_N_2),
+      (p_0, p_1) =>
+        FunCall(Join(),
+          FunCall(MapGlb(1)(fun((p_2) =>
+            FunCall(TransposeW(),
+              FunCall(MapGlb(0)(fun((p_3) =>
+                FunCall(TransposeW(),
+                  FunCall(toGlobal(fun((p_4) =>
+                    FunCall(MapSeq(fun((p_5) =>
+                      FunCall(MapSeq(fun((p_6) =>
+                        FunCall(idfloat, p_6))), p_5))), p_4))),
+                    FunCall(ReduceSeq(fun((p_7, p_8) =>
+                      FunCall(MapSeq(fun((p_9) =>
+                        FunCall(add,
+                          FunCall(Get(0), p_9),
+                          FunCall(mult,
+                            FunCall(Get(1), p_9),
+                            FunCall(Get(1), p_8))))),
+                        FunCall(Zip(2), p_7,
+                          FunCall(Get(0), p_8))))),
+                      FunCall(MapSeq(fun((p_10) =>
+                        FunCall(idfloat, p_10))), Value("0.0f", ArrayType(Float, v__3))),
+                      FunCall(Zip(2),
+                        FunCall(Transpose(), p_2), p_3)))))), p_1)))),
+            FunCall(Split(v__3), p_0))))
+
+    printStrategicLocations(f)
+  }
+
+  @Test
+  def twoDRegBlock(): Unit = {
+    val v_K_0 = Var("K", StartFromRange(1))
+    val v_M_1 = Var("M", StartFromRange(1))
+    val v_N_2 = Var("N", StartFromRange(1))
+    val v__3 = Var("", RangeMul(1,1+v_M_1,2))
+    val v__4 = Var("", RangeMul(1,1+v_N_2,2))
+
+    val f = fun(
+      ArrayType(ArrayType(Float, v_K_0), v_M_1),
+      ArrayType(ArrayType(Float, v_K_0), v_N_2),
+      (p_0, p_1) =>
+        FunCall(Map(fun((p_2) =>
+          FunCall(Scatter(ReorderWithStride(v_N_2 / v__4)), p_2))),
+          FunCall(Join(),
+            FunCall(MapGlb(1)(fun((p_3) =>
+              FunCall(TransposeW(),
+                FunCall(Join(),
+                  FunCall(MapGlb(0)(fun((p_4) =>
+                    FunCall(TransposeW(),
+                      FunCall(Map(fun((p_5) =>
+                        FunCall(TransposeW(), p_5))),
+                        FunCall(TransposeW(),
+                          FunCall(toGlobal(fun((p_6) =>
+                            FunCall(MapSeq(fun((p_7) =>
+                              FunCall(MapSeq(fun((p_8) =>
+                                FunCall(MapSeq(fun((p_9) =>
+                                  FunCall(idfloat, p_9))), p_8))), p_7))), p_6))),
+                            FunCall(ReduceSeq(fun((p_10, p_11) =>
+                              FunCall(MapSeq(fun((p_12) =>
+                                FunCall(MapSeq(fun((p_13) =>
+                                  FunCall(add,
+                                    FunCall(Get(0), p_13),
+                                    FunCall(mult,
+                                      FunCall(Get(1), p_12),
+                                      FunCall(Get(1), p_13))))),
+                                  FunCall(Zip(2),
+                                    FunCall(Get(0), p_12),
+                                    FunCall(Get(1), p_11))))),
+                                FunCall(Zip(2), p_10,
+                                  FunCall(Get(0), p_11))))),
+                              FunCall(MapSeq(fun((p_14) =>
+                                FunCall(MapSeq(fun((p_15) =>
+                                  FunCall(idfloat, p_15))), p_14))), Value("0.0f", ArrayType(ArrayType(Float, v__4), v__3))),
+                              FunCall(Zip(2),
+                                FunCall(Transpose(), p_3),
+                                FunCall(Transpose(), p_4))))))))),
+                    FunCall(Split(v__4),
+                      FunCall(Gather(ReorderWithStride(v_N_2 / v__4)), p_1))))))),
+              FunCall(Split(v__3), p_0)))))
+
+    println(f)
+    getReuseCandidates(f)
+  }
+
+  @Test
+  def tilingAndOneDRegBlock(): Unit = {
+
+  }
+
+  @Test
+  def tilingAndTwoDRegBlock(): Unit = {
+
+  }
+}
