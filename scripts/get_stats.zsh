de86938011058ec35bf12eae4dac3f89c5a116c5
Toomas Remmelg <toomas.remmelg@ed.ac.uk>
2017-11-10 09:59:33 +0000

    Fix looping over scripts arguments and collect user function counts

--- scripts/get_stats.zsh
+++ scripts/get_stats.zsh
@@ -1,25 +1,29 @@
 #!/usr/bin/env zsh
 
-ALL_BENCHMARKS=$argv
-
 mkdir -p stats
 
-for BENCHMARK_FOLDER in $ALL_BENCHMARKS
+for BENCHMARK_FOLDER in "$@"
 do
 
   BENCHMARK=$(sed 's/Cl//g' <<< $BENCHMARK_FOLDER)
 
   echo $BENCHMARK
 
-  STATS_FILES=$(find $BENCHMARK_FOLDER -name "stats*" | cut -d / -f 3 | sort  | uniq | sed '/^$/d')
-  STATS_FILES=("${(@f)$(echo $STATS_FILES)}")
+  SIZES=$(find $BENCHMARK_FOLDER -name "stats*" | cut -d / -f 3 | sort  | uniq | sed -e '/^$/d' -e 's/stats//g')
+  SIZES=("${(@f)$(echo $SIZES)}")
 
   mkdir -p stats/$BENCHMARK
 
-  for SIZE in $STATS_FILES
+  for SIZE in $SIZES
   do
-    DESTINATION=stats/$BENCHMARK/$SIZE
-    cat $BENCHMARK_FOLDER/stats_header.csv > $DESTINATION
-    find $BENCHMARK_FOLDER -name $SIZE | xargs cat >> $DESTINATION
+    STATS_FILENAME=stats$SIZE
+    STATS_DESTINATION=stats/$BENCHMARK/$STATS_FILENAME
+
+    cat $BENCHMARK_FOLDER/stats_header.csv > $STATS_DESTINATION
+    find $BENCHMARK_FOLDER -name $STATS_FILENAME | xargs cat >> $STATS_DESTINATION
+
+    USERFUNS_FILENAME=userfuns$SIZE
+    USERFUNS_DESTINATION=stats/$BENCHMARK/$USERFUNS_FILENAME
+    find $BENCHMARK_FOLDER -name $USERFUNS_FILENAME | xargs cat > $USERFUNS_DESTINATION
   done
 done
