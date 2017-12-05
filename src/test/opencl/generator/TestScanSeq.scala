f6e542c168afda9ac838f2aa116c56123a04f6ff
federico <pizzutifederico27@gmail.com>
2017-12-05 14:25:59 +0000

    More tests for ScanSeq

--- src/test/opencl/generator/TestScanSeq.scala
+++ src/test/opencl/generator/TestScanSeq.scala
@@ -30,11 +30,14 @@ class TestScanSeq {
     val scalaResult = input.scan(0.0f)((x, y) => x + y).tail
     val zipped = scalaResult.zip(dOutput)
     zipped.foreach(x => assert(x._1 == x._2))
-    zipped.foreach(println)
   }
 
+  @Test
   def testInsideMapAndSplits() = {
     val N = 1024
+
+    val input = Array.fill(N)(1.0f)
+    val scalaResult = input.grouped(32).map(_.scan(0.0f)((x, y) => x + y).tail).flatten.toArray
 
     val expr  =
      fun(ArrayTypeWSWC(Float, N),
@@ -42,64 +45,71 @@ class TestScanSeq {
          toGlobal(MapSeq(id)) o Join() o MapSeq(ScanSeq(add, 0.0f)) o Split(32) $ data
        })
 
-    val input = Array.fill(N)(1.0f)
-    val (dOutput, _) = Execute(128)[Array[Float]](expr, input)
-  }
-
-  def testSplitJoin() = {
-    val N = 1024
-
-    val expr = fun(ArrayTypeWSWC(Float, N),
-      data => toGlobal(MapSeq(id)) o ScanSeq(add, 0.0f) o Join() o Split(32) $ data)
-
-    val input = Array.fill(N)(1.0f)
     val (dOutput, _) = Execute(128)[Array[Float]](expr, input)
 
-    dOutput.foreach(x => println(s"$x"))
+    val zipped = scalaResult.zip(dOutput)
+    zipped.foreach(x => assert(x._1 == x._2))
   }
-
 
   @Test
   def testScanAcrossArraysGlobal() = {
     val N = 1024
     val SS = 16
 
+    val input = Array.fill(N)(1.0f)
+    val scalaResult = input.grouped(SS).scanLeft(Array.fill(SS)(0.0f))((x, y) => x.zip(y).map(x => x._1 + x._2)).toArray.tail.flatten
+
     def joinArray(S:ArithExpr) =
       fun(ArrayTypeWSWC(Float,S), ArrayTypeWSWC(Float,S), (arr1,arr2) => MapSeq(add) $ Zip(arr1, arr2))
 
     val expr = fun(ArrayTypeWSWC(Float, N),
-      data => toGlobal(MapSeq(MapSeq(id))) o
+      data => Join() o MapGlb(toGlobal(MapSeq(id)) o Join() o
         ScanSeq(
           joinArray(SS),
-          toGlobal(MapSeq(id)) $ Value(1.0f, ArrayTypeWSWC(Float, SS))
-        ) o Split(SS) $ data
+          toGlobal(MapSeq(id)) $ Value(0.0f, ArrayTypeWSWC(Float, SS))
+        ) o Split(SS)) o Split(N) $ data
     )
 
-    val input = Array.fill(N)(1.0f)
     val (dOutput, _) = Execute(1024)[Array[Float]](expr, input)
 
-    dOutput.foreach(x => println(s"$x"))
+    dOutput.foreach(println)
+    println("-----")
+    scalaResult.foreach(println)
+    val zipped = scalaResult.zip(dOutput)
+    zipped.foreach(x => assert(x._1 == x._2))
   }
 
   @Test
-  def testScanAcrossArraysPrivate() = {
+  def testScanAcrossArraysLocal() = {
     val N = 1024
     val SS = 16
 
+    val input = Array.fill(N)(1.0f)
+    val scalaResult = input.grouped(SS).scanLeft(Array.fill(SS)(0.0f))((x, y) => x.zip(y).map(x => x._1 + x._2)).toArray.tail.flatten
+
     def joinArray(S:ArithExpr) =
-      fun(ArrayTypeWSWC(Float,S), ArrayTypeWSWC(Float,S), (arr1,arr2) => MapSeq(add) $ Zip(arr1, arr2))
+      fun(ArrayTypeWSWC(Float,S), ArrayTypeWSWC(Float,S), (arr1,arr2) => MapSeq(toLocal(add)) $ Zip(arr1, arr2))
 
     val expr = fun(ArrayTypeWSWC(Float, N),
-      data => toGlobal(MapSeq(MapSeq(id))) o
-        ScanSeq(
-          joinArray(SS),
-          MapSeq(id) $ Value(1.0f, ArrayTypeWSWC(Float, SS))
-        ) o Split(SS) $ data
+      data => Join() o
+        MapWrg(Join() o
+          MapLcl(
+            MapSeq(toGlobal(id)) o Join() o
+              ScanSeq(
+                joinArray(SS),
+                MapSeq(toLocal(id)) $ Value(0.0f, ArrayTypeWSWC(Float, SS)))
+              o Split(SS)
+          )
+          o Split(N)
+        ) o Split(N) $ data
     )
 
-    val input = Array.fill(N)(1.0f)
-    val (dOutput, _) = Execute(1024)[Array[Float]](expr, input)
+    val (dOutput, _) = Execute(1,1)[Array[Float]](expr, input)
 
-    dOutput.foreach(x => println(s"$x"))
+    dOutput.foreach(println)
+    println("-----")
+    scalaResult.foreach(println)
+    val zipped = scalaResult.zip(dOutput)
+    zipped.foreach(x => assert(x._1 == x._2))
   }
 }
