f44939622d75200caa3ae739ecc8e40b7ebe424b
Maurice Heine <maurice.heine@gmx.net>
2018-01-21 12:01:39 +0100

    Fixed Y scaling.

--- src/test/utils/TestVisualization.scala
+++ src/test/utils/TestVisualization.scala
@@ -19,7 +19,7 @@ class TestVisualization{
     def psiPaperTestExpression(): Unit = {
         def M = Var("M")
         def N = Var("N")
-        def expression =   Join() o  Map(Reduce(add, 0.0f))o PrintType(true)   o Split(M)
+        def expression =   Join() o  PrintType(true)   o Map(Reduce(add, 0.0f))o  Split(M)
 
                 val lambda = \(ArrayType(Float, N), input => expression $ input)
         TypeChecker(lambda)
