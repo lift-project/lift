db121521940b706bbf515dc7363fd73d8547a1e6
Adam Harries <adam.harries@ed.ac.uk>
2018-02-15 13:29:53 +0000

    remove unecessary printer class

--- src/main/utils/Printer.scala
+++ src/main/utils/Printer.scala
@@ -7,48 +7,6 @@ import opencl.generator.{NotPrintableExpression, OclFunction}
 
 // TODO: Move much of this code to the new generic.AstPrinter module
 
-abstract class Printer {
-  /** Output stream */
-  val sb: StringBuilder = new StringBuilder
-  /** Current indentation (depth of scope) */
-  var indent: Int = 0
-  val tabSize = 2
-
-  def print(s: String): Unit = {
-    sb ++= s
-  }
-
-  /** Print the given string and create an indented new line */
-  def println(s: String): Unit = {
-    sb ++= s + "\n" + tab()
-  }
-
-  /** Create a block between braces. */
-  def printBlock(code: => Unit): Unit = {
-    indent += 1
-    println("{")
-    code
-    indent -= 1
-    moveCursorBack(tabSize)
-    print("}")
-  }
-
-  /** Insert the correct indentation */
-  def tab(): String = {
-    lazy val whiteSpace: String = " " * tabSize
-    whiteSpace * indent
-  }
-
-  /** Move cursor back by given size. Used to fix indentation */
-  def moveCursorBack(size: Int): Unit = {
-    for (_ <- 1 to size) {
-      if (sb.last.isWhitespace) {
-        sb.deleteCharAt(sb.size - 1)
-      }
-    }
-  }
-}
-
 object Printer {
 
   def toString(t: Type, seenArray: Boolean = false): String = {
