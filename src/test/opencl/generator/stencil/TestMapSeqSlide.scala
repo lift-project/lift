fda69afa16f25dd66b2183f538405e26956fa099
Adam Harries <adam.harries@ed.ac.uk>
2018-02-15 17:50:13 +0000

    remove ignore, as test now passes

--- src/test/opencl/generator/stencil/TestMapSeqSlide.scala
+++ src/test/opencl/generator/stencil/TestMapSeqSlide.scala
@@ -637,7 +637,7 @@ class TestMapSeqSlide
     assertArrayEquals(gold, output, 0.1f)
 
   }
-  @Test @Ignore
+  @Test
   def reduceSlide3DTest27PointWithWeights(): Unit = {
     assumeFalse("Disabled on Apple OpenCL Platform.", Utils.isApplePlatform)
 
