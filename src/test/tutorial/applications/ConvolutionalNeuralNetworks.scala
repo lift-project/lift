c29afe19eb141da53a63e127b609fcb8fb0ab297
larisa <larisa.stoltzfus@ed.ac.uk>
2018-03-26 17:46:34 +0100

    renaming !

---
+++ src/test/tutorial/applications/ConvolutionalNeuralNetworks.scala
@@ -0,0 +1,14 @@
+package tutorial.applications
+
+import opencl.executor.TestWithExecutor
+
+/**
+  * Here is a test class showing an example of a convolutional neural network
+  */
+
+object ConvolutionalNeuralNetworks extends TestWithExecutor
+
+class ConvolutionalNeuralNetworks
+{
+
+}
