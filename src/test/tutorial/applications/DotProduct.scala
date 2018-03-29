fa513b4aa83b441ca1c854ddc9342c2015a96b0d
naummo <naums.mogers@gmail.com>
2018-03-29 05:26:18 +0100

    Added another dotProduct example for the tutorial

--- src/test/tutorial/applications/DotProduct.scala
+++ src/test/tutorial/applications/DotProduct.scala
@@ -1,13 +1,14 @@
 package tutorial.applications
 
 import ir.ArrayTypeWSWC
-import ir.ast.{Join, Split, Zip, fun}
+import ir.ast.{Get, Iterate, Join, Split, Zip, fun}
+import ir.printer.DotPrinter
 import lift.arithmetic._
 import opencl.executor._
-import org.junit.Test
-import org.junit.Assert._
 import opencl.ir._
 import opencl.ir.pattern._
+import org.junit.Assert._
+import org.junit.Test
 
 /**
   * Here is an example test case of dot product.
@@ -32,7 +33,6 @@ class DotProduct {
   /*
    * original dot product
    */
-
   @Test def simpleDotProduct(): Unit = {
 
     def dotProductScala(left: Array[Float], right: Array[Float]): Float = {
@@ -44,7 +44,12 @@ class DotProduct {
         ArrayTypeWSWC(Float, N),
         ArrayTypeWSWC(Float, N),
         (left, right) => {
-          Join() o MapGlb(toGlobal(MapSeq(id)) o ReduceSeq(add, 0.0f) o MapSeq(mult)) o Split(4) $ Zip(left, right)
+          Join() o 
+            MapGlb(
+              toGlobal(MapSeq(id)) o
+                ReduceSeq(add, 0.0f) o
+                MapSeq(mult)) o
+            Split(4) $ Zip(left, right)
       })
 
     val (output, runtime) = Execute(inputSize)[Array[Float]](dotProductLift, leftInputData, rightInputData)
@@ -54,17 +59,21 @@ class DotProduct {
   }
 
   /*
-   * dot product utlising workgroups and local memory
+   * dot product utilising workgroups and local memory
    */
-
-  @Test def dotProductWithLocalAndWorkgroupMappings (): Unit = {
+  @Test def dotProductWithLocalAndWorkgroupMappings(): Unit = {
 
     val dotProductLift = fun(
       ArrayTypeWSWC(Float, N),
       ArrayTypeWSWC(Float, N),
       (left, right) => {
         Join() o MapWrg(
-          Join() o  MapLcl(toGlobal(MapSeq(id)) o ReduceSeq(add, 0.0f) o MapSeq(mult)) o Split(4)
+          Join() o 
+            MapLcl(
+              toGlobal(MapSeq(id)) o
+                ReduceSeq(add, 0.0f) o
+                MapSeq(mult)) o 
+            Split(4)
         ) o Split(1024) $ Zip(left, right)
       })
 
@@ -72,6 +81,36 @@ class DotProduct {
 
     assertEquals(dotProductScala(leftInputData, rightInputData), output.sum, 0.0)
 
+  }
+
+  /*
+   * dot product utilising workgroups, local memory and iterate
+   */
+  @Test def dotProductWithLocalAndWorkgroupMappingsAndIterate(): Unit = {
+
+    val dotProductLift = fun(
+      ArrayTypeWSWC(Float, N),
+      ArrayTypeWSWC(Float, N),
+      (left, right) => {
+        Join() o MapWrg(
+          Join() o MapLcl(toGlobal(MapSeq(id))) o Split(1) o
+            Iterate(6)(
+              Join() o
+                MapLcl(toLocal(MapSeq(id)) o ReduceSeq(add, 0.0f)) o
+                Split(2)) o Join() o
+            MapLcl(
+              toLocal(MapSeq(id)) o
+                ReduceSeq(fun((acc, y) => multAndSumUp(acc, Get(y, 0), Get(y, 1))), 0.0f)) o 
+            Split(2)
+        ) o Split(128) $ Zip(left, right)
+      })
+
+
+    val (output, runtime) = Execute(inputSize)[Array[Float]](dotProductLift, leftInputData, rightInputData)
+
+    assertEquals(dotProductScala(leftInputData, rightInputData), output.sum, 0.0)
+
+    DotPrinter("./", "expr", dotProductLift)
   }
 
 }
