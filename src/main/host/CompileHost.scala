4c676f5d62dea1dc3ac82875e6413e1ae8d11c29
Lu Li <explore.leo@gmail.com>
2018-11-16 11:18:01 +0000

    extend IRNode with visit and visitAndRebuild facility

--- src/main/host/CompileHost.scala
+++ src/main/host/CompileHost.scala
@@ -1,11 +1,26 @@
 package host
 
-import ir.ast.Lambda
+import ir.{TypeChecker, UndefType}
+import ir.ast.{Expr, IRNode, Lambda}
 
 object CompileHost {
 
   def apply(lambda: Lambda, path: String) ={
 
+    val final_lambda = lambda.asInstanceOf[Lambda]
+
+    final_lambda.params.foreach(p => assert(p.t != UndefType))
+    TypeChecker(final_lambda)
+
+    lambda.visit( pre = {node:IRNode =>
+      node match {
+        case e:Expr => assert( e.view != UndefType )
+        case _ =>
+      }
+    })
+
+
+    println("All done!")
   }
 
 
--- src/main/ir/ast/IRNode.scala
+++ src/main/ir/ast/IRNode.scala
@@ -1,3 +1,53 @@
 package ir.ast
 
-trait IRNode
+import java.util.concurrent.atomic.AtomicInteger
+
+object IDGenerator {
+  val id_counter = new AtomicInteger(0)
+  def get_id() = id_counter.incrementAndGet()
+}
+
+class IgnoreChildrenExceptionUnit() extends Exception
+class IgnoreChildrenException(val result: IRNode) extends Exception
+
+trait IRNode {
+  final val gid = IDGenerator.get_id()
+
+  final def visit(pre: IRNode => Unit = {n => }, post: IRNode => Unit = {n => }) : Unit = {
+    this.visit( (n:IRNode) => {
+      try {
+        pre(n)
+        val prePost : IRNode => IRNode => Unit = { n:IRNode => pre(n); post }
+        this._visit(prePost)
+      } catch {
+        case iceu: IgnoreChildrenExceptionUnit =>
+          post(n)
+          throw iceu
+      }
+    } )
+  }
+
+  def _visit(prePost: IRNode => IRNode => Unit) : Unit = ()
+
+  final def visit(prePost: IRNode => IRNode => Unit) : Unit = {
+    try {
+      val post: IRNode => Unit = prePost(this)
+      this._visit(prePost)
+      post(this)
+    } catch {
+      case _ : IgnoreChildrenExceptionUnit => ()
+    }
+  }
+
+  final def visitAndRebuild(pre: IRNode => IRNode = {n => n}, post: IRNode => IRNode = {n =>n} ) : IRNode =
+    post( try {pre(this)._visitAndRebuild(pre,post)}
+    catch {
+      case ice : IgnoreChildrenException => ice.result // skipping the children and returning result of pre stored in the exception
+    }
+    )
+
+  def _visitAndRebuild(pre: IRNode => IRNode, post: IRNode => IRNode) : IRNode = this
+
+
+}
+
