040487f47d45e409a6201e801f7cb8077f60e441
Naums Mogers <naums.mogers@gmail.com>
2018-12-17 14:20:29 +0000

    Added a high-level expression for the ReLU layer

---
+++ src/main/rewriting/macrorules/nn/ReLU.scala
@@ -0,0 +1,38 @@
+package rewriting.macrorules.nn
+
+import ir.ArrayType
+import ir.ast.{FunDecl, Map, UserFun, λ}
+import lift.arithmetic.SizeVar
+import opencl.ir.{Float, id}
+
+object ReLU {
+
+  val ReLUFun: UserFun = UserFun("ReLU", "x", "{ return(max(0.0f, x)); }", Float, Float)
+
+  val nChannels = SizeVar("nChannels")
+  val spatialSize = SizeVar("spatialSize")
+  val nInputs = SizeVar("nInputs")
+  val nBatches = SizeVar("nBatches")
+
+  def expr: FunDecl = {
+    λ(ArrayType(ArrayType(ArrayType(ArrayType(ArrayType(Float,
+      nChannels), spatialSize), spatialSize), nInputs), nBatches),
+      (X) => {
+        Map(λ(batch =>
+          Map(λ(singleInput =>
+            Map(λ(firstSpatialDimElement =>
+
+              Map(λ(secondSpatialDimElement =>
+
+                Map(λ(elementChannel =>
+                  ReLUFun(elementChannel)
+                )) $ secondSpatialDimElement
+
+              )) $ firstSpatialDimElement
+
+            )) $ singleInput
+          )) $ batch
+        )) $ X
+      })
+  }
+}
