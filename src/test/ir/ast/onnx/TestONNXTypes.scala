409c876fbc599e0cad4183fc1c6f9542e37a5624
Lu Li <explore.leo@gmail.com>
2019-01-09 18:08:22 +0000

    IR construct works now for ONNX for convolution layer

--- src/test/ir/ast/onnx/TestONNXTypes.scala
+++ src/test/ir/ast/onnx/TestONNXTypes.scala
@@ -1,17 +1,43 @@
 package ir.ast.onnx
 
-import ir.{ArrayType, ArrayTypeWS}
-import ir.ast.fun
+import ir.ArrayType
+import ir.ast.{Lambda, fun}
 import lift.arithmetic.Cst
-import org.junit.Test
 import opencl.ir.Float
+import org.junit.Test
+
+import scala.reflect.runtime._
+import scala.tools.reflect.ToolBox
+import scala.io.Source
 
 class TestONNXTypes {
 
   @Test
   def test_Conv() : Unit = {
 
-    val f = fun(ArrayType(Float,List(Cst(1),Cst(3),Cst(20),Cst(20))),  ArrayType(Float,List(Cst(5),Cst(3),Cst(4),Cst(4))), (X,W)=> {ConvWithoutBias(auto_pad = "NOTSET",dilations = List(0,0),group = 1,kernel_shape = List(3,4,4),pads = List(1,1),strides = List(1,1)) (X,W)})
+    //val f = fun(ArrayType(Float,List(Cst(1),Cst(3),Cst(20),Cst(20))),  ArrayType(Float,List(Cst(5),Cst(3),Cst(4),Cst(4))), (X,W)=> {ConvWithoutBias(auto_pad = "NOTSET",dilations = List(0,0),group = 1,kernel_shape = List(3,4,4),pads = List(1,1),strides = List(1,1)) (X,W)})
+
+    val prologue =
+      """
+        | import ir.{ArrayType}
+        | import ir.ast.fun
+        | import lift.arithmetic.Cst
+        | import opencl.ir.Float
+        | import ir.ast.onnx._
+        |
+        |
+      """.stripMargin
+
+    val expr = Source.fromFile("/tmp/lu_lift_expr_from_onnx.txt").getLines.mkString
+
+    val code = prologue + expr
+
+
+    val mirror = universe.runtimeMirror(getClass.getClassLoader)
+    val tb = mirror.mkToolBox()
+    val tree = tb.parse(code)
+    val f = tb.eval(tree).asInstanceOf[Lambda]
+
 
     /*
     val f = fun(
