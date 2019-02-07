94b91d034e10370ed7ada57aaf29121cb8cf6f4c
Lu Li <explore.leo@gmail.com>
2019-02-07 11:46:39 +0000

    bug fix

--- src/test/cbackends/host/25.conv3d_slide3D_diff/main.cpp
+++ src/test/cbackends/host/25.conv3d_slide3D_diff/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 	int const N = 8;
 	int const output_N = 3;
 	int const feature_size=6;
-	vector<float> in(N*N*N,1.0f), weights(feature_size*feature_size*feature_size, 2.0f);
+	vector<float> in(N*N*N,1.0f), weights(feature_size*feature_size*N, 2.0f);
 	float *out = nullptr;
 
         /* std::cout << "output size = " << (-8 + (-6 * (int)pow((float)N, 2)) + (12 * N) + (int)pow((float)N, 3)) << std::endl; */
