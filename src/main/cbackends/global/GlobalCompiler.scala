b5a1c78f36236a1f55d95231cfa42b4938336ebe
Lu Li <explore.leo@gmail.com>
2019-03-22 16:57:52 +0000

    add some initial code to tackle opencl's memory objects, used for generate function call to opencl function declaration

--- src/main/cbackends/global/GlobalCompiler.scala
+++ src/main/cbackends/global/GlobalCompiler.scala
@@ -7,6 +7,7 @@ import cbackends.global.lowering.{GenerateCLockPrintingStmt, GenerateGlobalClock
 import cbackends.global.transformation.cast_transformation.cpu_outline_transformation.{CPUOutlineTargetAnalysis, OclOutlineTargetAnalysis}
 import cbackends.global.transformation.empty_kernel_structure.EmptyKernelStructure
 import cbackends.global.transformation.funcall2closure.FunCall2Closure
+import cbackends.global.transformation.ocl_memory_gen.OclMemoryGen
 import cbackends.global.transformation.purify.Purify
 import cbackends.global.transformation.unique_user_func.UniqueUserFunc
 import cbackends.host.HostCompiler
@@ -56,6 +57,9 @@ object GlobalCompiler{
         val oclfundefs = all_oclfunc_outline_targets_purified.map {
           case (filename:String, lambdax:Lambda) =>  (filename , ( opencl.executor.Compile.!!(lambdax) ) )
         }
+
+        OclMemoryGen(lambda)
+
         //val final_oclfundefs = UniqueUserFunc(oclfundefs)
         val final_oclfundefs = oclfundefs
 
