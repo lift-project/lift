dd411c6b9576f95f44d6af3c7545b53c0165afd4
Larisa Stoltzfus <larisa.stoltzfus@ed.ac.uk>
2019-03-22 18:01:42 +0000

    initialising fred

---
+++ src/main/ir/ast/Fred.scala
@@ -0,0 +1,74 @@
+package ir.ast
+
+import ir._
+import ir.interpreter.Interpreter.ValueMap
+
+/**
+ * Fred pattern.
+ * Code for this pattern can be generated.
+ *
+ * The fred pattern has the following high-level semantics:
+ *   <code>Fred(2)( [x,,1,,, ..., x,,n,,], [y,,1,,, ..., y,,n,,] )
+ *      = [ (x,,1,,, y,,1,,), ..., (x,,n,,, y,,n,,) ]</code>
+ * The definitions for `n > 2` are accordingly.
+ *
+ * The fred pattern has the following type:
+ *   `Fred(2) : [a],,i,, -> [b],,i,, -> [a x b],,i,,`
+ * The definitions for `n > 2` are accordingly.
+ *
+ * @param n The number of arrays which are combined. Must be >= 2.
+ */
+case class Fred(n : Int) extends Pattern(arity = n) {
+
+  override def checkType(argType: Type,
+                         setType: Boolean): Type = {
+    argType match {
+      case tt: TupleType =>
+        if (tt.elemsT.length != n) throw new NumberOfArgumentsException
+
+        if (! tt.elemsT.forall( t => t.isInstanceOf[ArrayType]) ) {
+          throw TypeException("")
+        }
+
+//        tt.elemsT.foldLeft(Set())( (set, t) => {
+//          if (!set.contains(t)) {
+  //          if(set.empty()){}
+            // check if empty
+            // if not add it
+    //      }
+     //   } )
+
+
+        ???
+
+        // 4 checks ( input is tuple -don't ask why)
+        // check x is array
+        // check y is array
+        // check x and y have same element types
+        // return array (element type, sum of length of x and y)
+        // check all element types in tuples are arrays and have same element type
+      case _ => throw new TypeException(argType, "TupleType", this)
+    }
+  }
+
+  override def eval(valueMap: ValueMap, args: Any*): Vector[_] = {
+      ???
+  }
+}
+
+object Fred {
+  /**
+   * Create an instance of the fred pattern.
+   * This function infers the number of arrays which are combined with the fred
+   * pattern.
+   *
+   * @param args The arrays to be combined with the fred pattern.
+   * @return An instance of the fred pattern combining the arrays given by `args`
+   */
+  def apply(args : Expr*) : Expr = {
+    assert(args.length >= 2)
+    Fred(args.length)(args:_*)
+  }
+
+}
+
---
+++ src/test/opencl/generator/stencil/TestFred.scala
@@ -0,0 +1,46 @@
+package opencl.generator.stencil
+
+import ir.ArrayTypeWSWC
+import ir.ast.{UserFun, Zip, fun}
+import lift.arithmetic.SizeVar
+import opencl.executor._
+import opencl.ir.pattern._
+import org.junit._
+import opencl.ir._
+import org.junit.Assert._
+
+object TestFred extends TestWithExecutor
+
+
+class TestFred
+{
+
+  /** 1D **/
+  @Test
+  def bringTogetherTwoMapSeq1D(): Unit = {
+
+
+    val input = Array.fill[Float](2)(1.0f)
+    val gold = Array(2.0f,2.0f,4.0f,4.0f)
+
+    // mapseq over array of 1s and multiply by 2
+    // mapseq over array of 1s and add 3
+
+    val mult2 = UserFun("mult2", "x", "{ return x*2; }", Float, Float)
+    val add3 = UserFun("add3", Array("x"), "{ return x+3; }", Seq(Float), Float)
+
+    val fred = fun(
+      ArrayTypeWSWC(Float, SizeVar("N")),
+      (input) =>
+       Fred(MapSeq(mult2) $ input, MapSeq(add3) $ input)
+    )
+
+    val (output : Array[Float], _) = Execute(2, 2)[Array[Float]](fred,input)
+    assertArrayEquals(gold, output, 0.1f)
+
+  }
+
+
+}
+
+
