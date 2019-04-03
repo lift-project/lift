bf71561acb5a00d4fe3e6ebdd67c757cd3c9275c
Naums Mogers <naums.mogers@gmail.com>
2019-04-03 17:40:27 +0100

    Fixed RangeValueGenerator to generate Cst instead of Int

--- src/main/utils/RangeValueGenerator.scala
+++ src/main/utils/RangeValueGenerator.scala
@@ -1,6 +1,6 @@
 package utils
 
-import lift.arithmetic.{Range, RangeAdd, RangeMul}
+import lift.arithmetic.{Cst, Range, RangeAdd, RangeMul}
 
 object RangeValueGenerator {
 
@@ -12,31 +12,32 @@ object RangeValueGenerator {
         // TODO: implement more robustly as RangeMul.numVals
         // This assumes that both stop and start are powers of mul
         log(stop, mul) - log(start, mul)
-      case r => throw new IllegalArgumentException(s"Range $r is not supported by getRandomValueInRange")
+      case r => throw new IllegalArgumentException(s"Range $r is not supported by rangeSize")
     }
   }
 
-  def generateSingleValue(range: Range, i: Int): Int = {
+  def generateSingleValue(range: Range, i: Int): Cst = {
     assert(i < rangeSize(range))
     range match {
-      case rAdd: RangeAdd =>    rAdd.start.evalInt + rAdd.step.evalInt * i
-      case rMul: RangeMul =>    rMul.start.evalInt * Math.pow(rMul.mul.evalInt, i).toInt
-      case r@_ =>               throw new NotImplementedError(s"Range $r is unsupported by RangeGenerator")
+      case rAdd: RangeAdd =>    Cst(rAdd.start.evalInt + rAdd.step.evalInt * i)
+      case rMul: RangeMul =>    Cst(rMul.start.evalInt * Math.pow(rMul.mul.evalInt, i).toInt)
+      case r@_ =>               throw new NotImplementedError(s"Range $r is not supported by RangeGenerator")
     }
   }
 
-  def generateAllValues(range: Range): Vector[Int] = {
+  def generateAllValues(range: Range): Vector[Cst] = {
     range match {
       case rAdd: RangeAdd =>
         val start = rAdd.start.evalInt
         val step = rAdd.step.evalInt
         val stop = rAdd.stop.evalInt
-        (start until stop by step).toVector
+        (start until stop by step).toVector.map(Cst(_))
       case rMul: RangeMul =>
         val start = rMul.start.evalInt
         val stop = rMul.stop.evalInt
         val mul = rMul.mul.evalInt
-        (start until (Math.log(stop) / Math.log(mul)).toInt).scan(start)((previousValue, _) => previousValue * mul).toVector
+        (start until (Math.log(stop) / Math.log(mul)).toInt).scan(start)((previousValue, _) =>
+          previousValue * mul).toVector.map(Cst(_))
 
       case r@_ =>
         throw new NotImplementedError(s"Range $r is unsupported by RangeGenerator")
