6a868786c442e9de745d381ae868d6f04c9fa96e
Lu Li <explore.leo@gmail.com>
2019-05-10 15:05:22 +0100

    add some support for ArrayAccess

--- src/main/cbackends/common/view/OutputView.scala
+++ src/main/cbackends/common/view/OutputView.scala
@@ -3,7 +3,7 @@ package cbackends.common.view
 import cbackends.common.common_ir.CPUNullMemory
 import cbackends.common.utils.common_view.GenerateViewForRawInOut
 import ir.{ArrayType, ArrayTypeWS, ArrayTypeWSWC}
-import ir.ast.{AbstractMap, AbstractPartRed, Array2DFromUserFunGenerator, Array3DFromUserFunGenerator, ArrayFromUserFunGenerator, Expr, FunCall, Get, IRNode, Iterate, Join, Lambda, Pad, Param, Slide, Split, Transpose, TransposeW, UserFun, Value, Zip, transpose}
+import ir.ast.{AbstractMap, AbstractPartRed, Array2DFromUserFunGenerator, Array3DFromUserFunGenerator, ArrayAccess, ArrayFromUserFunGenerator, Expr, FunCall, Get, IRNode, Iterate, Join, Lambda, Pad, Param, Slide, Split, Transpose, TransposeW, UserFun, Value, Zip, transpose}
 import ir.view._
 import lift.arithmetic.{ArithExpr, Cst}
 import core.generator.PrettyPrinter._
@@ -53,7 +53,7 @@ object OutputView {
 
       }
 
-      case fc@FunCall(Get(n), arg)  => {
+      case fc@FunCall(Get(_)|ArrayAccess(_), arg)  => {
 
         assert(fc.outputView != NoView)
 
