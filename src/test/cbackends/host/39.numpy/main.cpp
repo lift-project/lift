4509cf1bfcb04e8e52022184f78703e419a69578
Lu Li <explore.leo@gmail.com>
2019-05-13 15:40:52 +0100

    the test framework is more or less done

--- src/test/cbackends/host/39.numpy/main.cpp
+++ src/test/cbackends/host/39.numpy/main.cpp
@@ -17,13 +17,14 @@ int main(int argc, char *argv[])
 	copy(y, y+size, ostream_iterator<float>(cout, " "));
 	std::cout << std::endl;
 
-	system("./main.py > python_golden.txt");
+	system("mkdir -p numpy_golden_data");
+	system("./numpy/sin.py > numpy_golden_data/sin.txt");
 
 
 	const int golden_data_size = size;
 	vector<float> golden_data(golden_data_size, -999.99);
 
-	ifstream file("python_golden.txt");
+	ifstream file("numpy_golden_data/sin.txt");
 
 	if (!file.good()) {
 		fprintf(stderr, "Could not open the data file.\n");
@@ -38,8 +39,10 @@ int main(int argc, char *argv[])
 	for(int i = 0; i < golden_data_size; ++i)
 	{
 		/* std::cout << abs( golden_data[i] - y[i] ) << std::endl; */
-		if( abs( golden_data[i] - y[i] ) > tol )
+		if( abs( golden_data[i] - y[i] ) > tol ){
 			std::cout << "Computed results does not match the golden data !!!" << std::endl;
+			exit(1);
+		}
 	}
 
 
