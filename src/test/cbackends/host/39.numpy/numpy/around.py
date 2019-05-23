fix a sign user func bug

src/test/cbackends/host/39.numpy/numpy/around.py
@@ -3,15 +3,9 @@
 import numpy as np
 
 
-y = np.around([0.37, 1.64]) 
+y = np.around([0.37, 1.64, .5, 0.6, 1.5, 2.5, 3.5, 4.5]) 
 
 z = list(map(str, y) )
 
 print(" ".join(z) )
-
-
-
-
-
-
 

src/test/cbackends/host/39.numpy/numpy/sign.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-y = np.sign([-1.,-2.,3.,4.]) 
+y = np.sign([0,-1.,-2.,3.,4.]) 
 
 z = list(map(str, y) )
 