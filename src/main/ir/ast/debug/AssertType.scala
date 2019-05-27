46ffcd1c92114a1466bb34f40966a90745e2892f
Naums <naums.mogers@gmail.com>
2019-05-27 19:31:03 +0100

    Enforced simplification for AssertExpr

--- src/main/ir/ast/debug/AssertType.scala
+++ src/main/ir/ast/debug/AssertType.scala
@@ -1,19 +1,24 @@
 package ir.ast.debug
 
 import ir.Type
+import lift.arithmetic.simplifier.ExprSimplifier
 
 /**
   * assertType checks if the given type is the same as the type of its parameter.
   */
 case class AssertType(expectedType: Type, name: String = "") extends
   TypeOperator(actualType => {
+
+    val expectedTypeSimplified = Type.visitAndRebuild(expectedType, ae => ExprSimplifier(ae))
+    val actualTypeSimplified = Type.visitAndRebuild(actualType, ae => ExprSimplifier(ae))
+
     try {
-      assert(expectedType.equals(actualType))
+      assert(expectedTypeSimplified.equals(actualTypeSimplified))
     } catch {
       case e: java.lang.AssertionError =>
         System.err.println("AssertType for \"" + name + "\" failed.\nExpected type:\n" +
-          expectedType.toString.replace("),", "),\n") +
-          "\nActual type:\n" + actualType.toString.replace("),", "),\n"))
+          expectedTypeSimplified.toString.replace("),", "),\n") +
+          "\nActual type:\n" + actualTypeSimplified.toString.replace("),", "),\n"))
         throw e
     }
   })
