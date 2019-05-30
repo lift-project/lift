70270ffcd7fe0722fbe3154df6c9832afc732d98
Naums <naums.mogers@gmail.com>
2019-05-30 14:31:07 +0100

    Disabled more tests

--- src/test/cbackends/host/TestHost.scala
+++ src/test/cbackends/host/TestHost.scala
@@ -18,6 +18,7 @@ import scala.sys.process._
 
 import cbackends.common.executor.Executor.{native_compile_and_run}
 
+@Ignore("These tests require fixes from host_code_gen_fft")
 class TestHost {
 
   val common_path = System.getProperty("user.dir") + "/src/test/cbackends/host"
@@ -548,7 +549,6 @@ class TestHost {
   // Another problem is that this test depended on Lu's change to Rules.scala:splitJoinMapSeq rule. Since that change
   // is breaking older tests, I reverted the rule to its original state. This test will have to be fixed to
   // account for that
-  @Ignore
   @Test
   def test_rewrite_rule_hello_world(): Unit = {
 
