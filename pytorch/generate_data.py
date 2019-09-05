ed0e971c76acec65e4f8d99ff09d57e1559e66c7
Naums Mogers <naums.mogers@arm.com>
2019-09-05 10:45:42 +0100

    Added LSTM training and testing in PyTorch for future functional comparison with Spatial

---
+++ pytorch/generate_data.py
@@ -0,0 +1,128 @@
+"""
+Code to generate autoregressive data.
+
+Blog post: http://www.jessicayung.com/generating-autoregressive-data-for-experiments=
+
+Author: Jessica Yung
+Sept 2018
+"""
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+class TimeSeriesData:
+    def __init__(self, num_datapoints, test_size=0.2, max_t=20, num_prev=1,
+                 noise_var=1):
+        """
+        Template class for generating time series data.
+        :param test_size: in (0,1), data to be used in test set as a fraction of all data generated.
+        """
+        self.num_datapoints = num_datapoints
+        self.test_size = test_size
+        self.num_prev = num_prev
+        self.max_t = max_t
+        self.data = None
+        self.noise_var = noise_var
+        self.y = np.zeros(num_datapoints + num_prev*4) # TODO: check this
+        self.bayes_preds = np.copy(self.y)
+
+        # Generate data and reshape data
+        self.create_data()
+
+        # Split into training and test sets
+        self.train_test_split()
+
+    def create_data(self):
+        self.generate_data()
+        self.reshape_data()
+
+    def generate_data(self):
+        """Generates data in self.y, may take as implicit input timesteps self.t.
+        May also generate Bayes predictions."""
+        raise NotImplementedError("Generate data method not implemented.")
+
+    def reshape_data(self):
+        self.x = np.reshape([self.y[i:i + self.num_prev] for i in range(
+            self.num_datapoints)], (-1, self.num_prev))
+        self.y = np.copy(self.y[self.num_prev:])
+        self.bayes_preds = np.copy(self.bayes_preds[self.num_prev:])
+
+    def train_test_split(self):
+        test_size = int(len(self.y) * self.test_size)
+        self.data = [self.X_train, self.X_test, self.y_train,
+                     self.y_test] = \
+                    self.x[:-test_size], self.x[-test_size:], \
+                    self.y[:-test_size], self.y[-test_size:]
+        self.bayes_preds = [self.bayes_train_preds, self.bayes_test_preds] = self.bayes_preds[:-test_size], self.bayes_preds[-test_size:]
+
+    def return_data(self):
+        return self.data
+
+    def return_train_test(self):
+        return self.X_train, self.y_train, self.X_test, self.y_test
+
+class ARData(TimeSeriesData):
+    """Class to generate autoregressive data."""
+
+    def __init__(self, *args, coeffs=None, **kwargs):
+        self.given_coeffs = coeffs
+        super(ARData, self).__init__(*args, **kwargs)
+
+        if coeffs is not None:
+            self.num_prev = len(coeffs) - 1
+
+    def generate_data(self):
+        self.generate_coefficients()
+        self.generate_initial_points()
+
+        # + 3*self.num_prev because we want to cut first (3*self.num_prev) datapoints later
+        # so dist is more stationary (else initial num_prev datapoints will stand out as diff dist)
+        for i in range(self.num_datapoints+3*self.num_prev):
+            # Generate y value if there was no noise
+            # (equivalent to Bayes predictions: predictions from oracle that knows true parameters (coefficients))
+            self.bayes_preds[i + self.num_prev] = np.dot(self.y[i:self.num_prev+i][::-1], self.coeffs)
+            # Add noise
+            self.y[i + self.num_prev] = self.bayes_preds[i + self.num_prev] + self.noise()
+
+        # Cut first 20 points so dist is roughly stationary
+        self.bayes_preds = self.bayes_preds[3*self.num_prev:]
+        self.y = self.y[3*self.num_prev:]
+
+    def generate_coefficients(self):
+        if self.given_coeffs is not None:
+            self.coeffs = self.given_coeffs
+        else:
+            filter_stable = False
+            # Keep generating coefficients until we come across a set of coefficients
+            # that correspond to stable poles
+            while not filter_stable:
+                true_theta = np.random.random(self.num_prev) - 0.5
+                coefficients = np.append(1, -true_theta)
+                # check if magnitude of all poles is less than one
+                if np.max(np.abs(np.roots(coefficients))) < 1:
+                    filter_stable = True
+            self.coeffs = true_theta
+
+    def generate_initial_points(self):
+        # Initial datapoints distributed as N(0,1)
+        self.y[:self.num_prev] = np.random.randn(self.num_prev)
+
+    def noise(self):
+        # Noise distributed as N(0, self.noise_var)
+        return self.noise_var * np.random.randn()
+
+# A set of coefficients that are stable (to produce replicable plots, experiments)
+fixed_ar_coefficients = {2: [ 0.46152873, -0.29890739],
+    5: [ 0.02519834, -0.24396899,  0.2785921,   0.14682383,  0.39390468],
+                        10: [-0.10958935, -0.34564819,  0.3682048,   0.3134046,  -0.21553732,  0.34613629,
+  0.41916508,  0.0165352,   0.14163503, -0.38844378],
+                         20: [ 0.1937815,   0.01201026,  0.00464018, -0.21887467, -0.20113385, -0.02322278,
+  0.34285319, -0.21069086,  0.06604683, -0.22377364,  0.11714593, -0.07122126,
+ -0.16346554,  0.03174824,  0.308584,    0.06881604,  0.24840789, -0.32735569,
+  0.21939492, 0.3996207 ]}
+
+"""
+Example of using fixed coefficients (consistency across tests of different models)
+
+data = ARData(100, coeffs=fixed_ar_coefficients[5], num_prev=5)
+plt.plot(data.y_train)
+"""
---
+++ pytorch/lstm-baseline.py
@@ -0,0 +1,242 @@
+"""
+Based on the code by Jessica Yung 
+Extended by Naums Mogers
+https://www.jessicayung.com/lstms-for-time-series-in-pytorch/
+"""
+import torch
+import torch.nn as nn
+from generate_data import *
+import matplotlib.pyplot as plt
+import argparse
+from os.path import join
+import pickle
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--train_out_dir", help="Directory for storing trained weights and generated inputs/outputs",
+                    type=str, required=True)
+action = parser.add_mutually_exclusive_group(required=True)
+action.add_argument('--train', action='store_true', help='Train the model and save weights, inputs and outputs to disk')
+action.add_argument('--test', action='store_true', 
+    help='Test the model by restoring weights from disk, producing new outputs and comparing with those in disk')
+
+args = parser.parse_args()
+
+
+#####################
+# Set parameters
+#####################
+
+# Data params
+noise_var = 0
+num_datapoints = 100
+test_size = 0.2
+num_train = int((1-test_size) * num_datapoints)
+num_test = num_datapoints - num_train
+
+# Network params
+input_size = 20
+# If `per_element` is True, then LSTM reads in one timestep at a time.
+per_element = True
+if per_element:
+    lstm_input_size = 1
+else:
+    lstm_input_size = input_size
+# size of hidden layers
+h1 = 32
+output_dim = 1
+num_layers = 2
+learning_rate = 1e-3
+num_epochs = 500
+dtype = torch.float
+
+weights_file_path = join(args.train_out_dir, "trained_lstm_weights.serial")
+inputs_file_path = join(args.train_out_dir, "lstm_inputs.pickle")
+gold_outputs_file_path = join(args.train_out_dir, "lstm_gold_outputs.pickle")
+pytorch_outputs_file_path = join(args.train_out_dir, "pytorch_gold_outputs.pickle")
+
+#####################
+# Build model
+#####################
+
+# Here we define our model as a class
+class LSTM(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, batch_size, output_dim=1,
+                    num_layers=2):
+        super(LSTM, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.set_batch_size(batch_size)
+        self.num_layers = num_layers
+
+        # Define the LSTM layer
+        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers)
+
+        # Define the output layer
+        self.linear = nn.Linear(self.hidden_dim, output_dim)
+
+    def set_batch_size(self, batch_size):
+        self.batch_size = batch_size
+
+    def init_hidden_state(self):
+        # This is what we'll initialise our hidden state as
+        return (torch.zeros(self.num_layers, self.batch_size, self.hidden_dim),
+                torch.zeros(self.num_layers, self.batch_size, self.hidden_dim))
+
+    def forward(self, input):
+        # Forward pass through LSTM layer
+        # shape of lstm_out: [input_size, batch_size, hidden_dim]
+        # shape of self.hidden: (a, b), where a and b both 
+        # have shape (num_layers, batch_size, hidden_dim).
+        lstm_out, self.hidden = self.lstm(input.view(len(input), self.batch_size, -1))
+        
+        # Only take the output from the final timetep
+        # Can pass on the entirety of lstm_out to the next layer if it is a seq2seq prediction
+        y_pred = self.linear(lstm_out[-1].view(self.batch_size, -1))
+        return y_pred.view(-1)
+
+
+def init_model():
+    return (
+        LSTM(lstm_input_size, h1, batch_size=num_train, output_dim=output_dim, num_layers=num_layers),
+        torch.nn.MSELoss(size_average=False))
+
+
+def train():
+    #####################
+    # Generate data
+    #####################
+    data = ARData(num_datapoints, num_prev=input_size, test_size=test_size, noise_var=noise_var, coeffs=fixed_ar_coefficients[input_size])
+
+    # make training and test sets in torch
+    X_train = torch.from_numpy(data.X_train).type(torch.Tensor)
+    X_test = torch.from_numpy(data.X_test).type(torch.Tensor)
+    y_train = torch.from_numpy(data.y_train).type(torch.Tensor).view(-1)
+    y_test = torch.from_numpy(data.y_test).type(torch.Tensor).view(-1)
+
+    X_train = X_train.view([input_size, -1, 1])
+    X_test = X_test.view([input_size, -1, 1])
+
+    #####################
+    # Train model
+    #####################
+
+    (model, loss_fn) = init_model()
+
+    optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    hist = np.zeros(num_epochs)
+
+    for t in range(num_epochs):
+        # Initialise hidden state
+        # Don't do this if you want your LSTM to be stateful
+        # model.hidden_state = model.init_hidden_state()
+        
+        # Forward pass
+        y_pred = model(X_train)
+
+        loss = loss_fn(y_pred, y_train)
+        if t % 100 == 0:
+            print("Epoch ", t, "MSE: ", loss.item())
+        hist[t] = loss.item()
+
+        # Zero out gradient, else they will accumulate between epochs
+        optimiser.zero_grad()
+
+        # Backward pass
+        loss.backward()
+
+        # Update parameters
+        optimiser.step()
+
+    #####################
+    # Test
+    #####################
+    y_test_pred = test(model, loss_fn, X_test, y_test)
+
+    #####################
+    # Save data to disk
+    #####################
+    backup_model_and_data(model, X_test, y_test, y_test_pred)
+
+    #####################
+    # Plot preds and performance
+    #####################
+
+    plt.plot(y_pred.detach().numpy(), label="Train preds")
+    plt.plot(y_train.detach().numpy(), label="Train targets")
+    plt.legend()
+    plt.show()
+
+    plt.plot(hist, label="Training loss")
+    plt.legend()
+    plt.show()
+
+
+def test(model, loss_fn, X_test, y_test, y_test_pred_post_train = None):
+    #####################
+    # Test
+    #####################
+    model.set_batch_size(num_test)
+
+    y_test_pred = model(X_test)
+    test_loss = loss_fn(y_test_pred, y_test)
+    if y_test_pred_post_train is not None:
+        test_loss_against_post_train_test = loss_fn(y_test_pred, y_test_pred_post_train)
+
+    print("Test MSE against golden outputs: ", test_loss.item())
+    if y_test_pred_post_train is not None:  
+        print("Test MSE against post-train PyTorch outputs: ", 
+            test_loss_against_post_train_test.item())
+    print("Test targets: ", y_test)
+    print("Test outputs: ", y_test_pred)
+
+    plt.plot(y_test_pred.detach().numpy(), label="Test preds", linewidth=7.0)
+    plt.plot(y_test.detach().numpy(), label="Test targets")
+    if y_test_pred_post_train is not None:  
+        plt.plot(y_test_pred_post_train.detach().numpy(), label="Post-train test preds", color="black")
+    plt.legend()
+    plt.show()
+
+    return y_test_pred
+
+
+def backup_model_and_data(model, X_test, y_test, y_test_pred):
+    #####################
+    # Save data to disk
+    #####################
+    torch.save(model.state_dict(), weights_file_path)
+    pickle.dump(X_test, open(inputs_file_path, "wb"))
+    pickle.dump(y_test, open(gold_outputs_file_path, "wb"))    
+    pickle.dump(y_test_pred, open(pytorch_outputs_file_path, "wb")) 
+
+
+def restore_model_and_data():
+    print("Restoring model and data from disk")
+
+    (model, loss_fn) = init_model()
+
+    model.load_state_dict(torch.load(weights_file_path))
+    model.eval()
+
+    X_test = pickle.load(open(inputs_file_path, "rb"))
+    y_test = pickle.load(open(gold_outputs_file_path, "rb"))            
+    y_test_pred_post_train = pickle.load(open(pytorch_outputs_file_path, "rb"))  
+
+    return (model, loss_fn, X_test, y_test, y_test_pred_post_train)
+
+    
+
+def test_without_training():
+    test(*restore_model_and_data())
+
+
+
+if args.train:
+    print("Training the model")
+    train()
+    
+if args.test:
+    print("Testing the model")
+    test_without_training()
