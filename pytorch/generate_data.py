1fd7e8800602240c9980fd8b202a05b5d69798d5
Naums Mogers <naums.mogers@arm.com>
2019-09-05 15:56:26 +0100

    Updated the pytorch example to accept vectorised inputs

--- pytorch/generate_data.py
+++ pytorch/generate_data.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 
 class TimeSeriesData:
     def __init__(self, num_datapoints, test_size=0.2, max_t=20, num_prev=1,
-                 noise_var=1):
+                 noise_var=1, vector_len=8):
         """
         Template class for generating time series data.
         :param test_size: in (0,1), data to be used in test set as a fraction of all data generated.
@@ -23,7 +23,8 @@ class TimeSeriesData:
         self.max_t = max_t
         self.data = None
         self.noise_var = noise_var
-        self.y = np.zeros(num_datapoints + num_prev*4) # TODO: check this
+        self.vector_len = vector_len
+        self.y = np.zeros(((num_datapoints + num_prev*4), self.vector_len)) # TODO: check this
         self.bayes_preds = np.copy(self.y)
 
         # Generate data and reshape data
@@ -42,18 +43,19 @@ class TimeSeriesData:
         raise NotImplementedError("Generate data method not implemented.")
 
     def reshape_data(self):
-        self.x = np.reshape([self.y[i:i + self.num_prev] for i in range(
-            self.num_datapoints)], (-1, self.num_prev))
-        self.y = np.copy(self.y[self.num_prev:])
-        self.bayes_preds = np.copy(self.bayes_preds[self.num_prev:])
+        self.x = np.reshape([self.y[i:i + self.num_prev, :] for i in range(
+            self.num_datapoints)], (-1, self.num_prev, self.vector_len))
+        self.y = np.copy(self.y[self.num_prev:, :])
+        self.bayes_preds = np.copy(self.bayes_preds[self.num_prev:, :])
 
     def train_test_split(self):
         test_size = int(len(self.y) * self.test_size)
         self.data = [self.X_train, self.X_test, self.y_train,
                      self.y_test] = \
-                    self.x[:-test_size], self.x[-test_size:], \
-                    self.y[:-test_size], self.y[-test_size:]
-        self.bayes_preds = [self.bayes_train_preds, self.bayes_test_preds] = self.bayes_preds[:-test_size], self.bayes_preds[-test_size:]
+                     self.x[:-test_size, :], self.x[-test_size:, :], \
+                     np.sum(self.y[:-test_size, :], axis=1), \
+                     np.sum(self.y[-test_size:, :], axis=1) # Xs are vectors, Ys are scalars (reduced vectors) (just an arbitrary dependence of Y->x)
+        self.bayes_preds = [self.bayes_train_preds, self.bayes_test_preds] = self.bayes_preds[:-test_size, :], self.bayes_preds[-test_size:, :]
 
     def return_data(self):
         return self.data
@@ -80,17 +82,18 @@ class ARData(TimeSeriesData):
         for i in range(self.num_datapoints+3*self.num_prev):
             # Generate y value if there was no noise
             # (equivalent to Bayes predictions: predictions from oracle that knows true parameters (coefficients))
-            self.bayes_preds[i + self.num_prev] = np.dot(self.y[i:self.num_prev+i][::-1], self.coeffs)
+            self.bayes_preds[i + self.num_prev, :] = np.sum(np.multiply(self.y[i:self.num_prev+i, :][::-1], self.coeffs), 
+                axis = 0)
             # Add noise
-            self.y[i + self.num_prev] = self.bayes_preds[i + self.num_prev] + self.noise()
+            self.y[i + self.num_prev, :] = self.bayes_preds[i + self.num_prev, :] + self.noise()
 
         # Cut first 20 points so dist is roughly stationary
-        self.bayes_preds = self.bayes_preds[3*self.num_prev:]
-        self.y = self.y[3*self.num_prev:]
+        self.bayes_preds = self.bayes_preds[3*self.num_prev:, :]
+        self.y = self.y[3*self.num_prev:, :]
 
     def generate_coefficients(self):
         if self.given_coeffs is not None:
-            self.coeffs = self.given_coeffs
+            coeff_list = self.given_coeffs
         else:
             filter_stable = False
             # Keep generating coefficients until we come across a set of coefficients
@@ -101,11 +104,18 @@ class ARData(TimeSeriesData):
                 # check if magnitude of all poles is less than one
                 if np.max(np.abs(np.roots(coefficients))) < 1:
                     filter_stable = True
-            self.coeffs = true_theta
+            coeff_list = true_theta
+
+        self.coeffs = np.zeros((self.num_prev, self.vector_len))
+        # Convert scalar coefficients into vectors
+        for i in range(self.num_prev):
+            self.coeffs[i, :] = np.array([coeff_list[i] + j*0.0000001 for j in range(self.vector_len)])
 
     def generate_initial_points(self):
         # Initial datapoints distributed as N(0,1)
-        self.y[:self.num_prev] = np.random.randn(self.num_prev)
+        r = np.random.randn(self.num_prev)
+        for i in range(self.num_prev):
+            self.y[i, :] = np.array([r[i]+ j*0.0000001 for j in range(self.vector_len)])
 
     def noise(self):
         # Noise distributed as N(0, self.noise_var)
--- pytorch/lstm-baseline.py
+++ pytorch/lstm-baseline.py
@@ -35,13 +35,14 @@ num_train = int((1-test_size) * num_datapoints)
 num_test = num_datapoints - num_train
 
 # Network params
-input_size = 20
+input_time_sequence_size = 20
+input_vector_length = 50
 # If `per_element` is True, then LSTM reads in one timestep at a time.
 per_element = True
 if per_element:
-    lstm_input_size = 1
+    lstm_input_sequence_size = 1
 else:
-    lstm_input_size = input_size
+    lstm_input_sequence_size = input_time_sequence_size
 # size of hidden layers
 h1 = 32
 output_dim = 1
@@ -63,16 +64,17 @@ pytorch_lstm0_out_file_path = join(args.train_out_dir, "pytorch_lstm0_outputs.pi
 # Here we define our model as a class
 class LSTM(nn.Module):
 
-    def __init__(self, input_dim, hidden_dim, batch_size, output_dim=1,
-                    num_layers=2):
+    def __init__(self, input_seq_size, hidden_dim, batch_size, output_dim=1,
+                    num_layers=2, vector_len=8):
         super(LSTM, self).__init__()
-        self.input_dim = input_dim
+        self.input_seq_size = input_seq_size
         self.hidden_dim = hidden_dim
         self.set_batch_size(batch_size)
         self.num_layers = num_layers
+        self.vector_len = vector_len
 
         # Define the LSTM layer
-        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers)
+        self.lstm = nn.LSTM(self.vector_len, self.hidden_dim, self.num_layers)
 
         # Define the output layer
         self.linear = nn.Linear(self.hidden_dim, output_dim)
@@ -90,7 +92,7 @@ class LSTM(nn.Module):
         # shape of lstm_out: [input_size, batch_size, hidden_dim]
         # shape of self.hidden: (a, b), where a and b both 
         # have shape (num_layers, batch_size, hidden_dim).
-        lstm_out, self.hidden = self.lstm(input.view(len(input), self.batch_size, -1))
+        lstm_out, self.hidden = self.lstm(input)
         
         # Only take the output from the final timetep
         # Can pass on the entirety of lstm_out to the next layer if it is a seq2seq prediction
@@ -100,7 +102,8 @@ class LSTM(nn.Module):
 
 def init_model():
     return (
-        LSTM(lstm_input_size, h1, batch_size=num_train, output_dim=output_dim, num_layers=num_layers),
+        LSTM(lstm_input_sequence_size, h1, batch_size=num_train, output_dim=output_dim, 
+            num_layers=num_layers, vector_len=input_vector_length),
         torch.nn.MSELoss(size_average=False))
 
 
@@ -108,8 +111,9 @@ def train():
     #####################
     # Generate data
     #####################
-    data = ARData(num_datapoints, num_prev=input_size, test_size=test_size, noise_var=noise_var, 
-        coeffs=fixed_ar_coefficients[input_size])
+    data = ARData(num_datapoints, num_prev=input_time_sequence_size, test_size=test_size, noise_var=noise_var,
+        vector_len=input_vector_length,
+        coeffs=fixed_ar_coefficients[input_time_sequence_size])
 
     # make training and test sets in torch
     X_train = torch.from_numpy(data.X_train).type(torch.Tensor)
@@ -117,8 +121,8 @@ def train():
     y_train = torch.from_numpy(data.y_train).type(torch.Tensor).view(-1)
     y_test = torch.from_numpy(data.y_test).type(torch.Tensor).view(-1)
 
-    X_train = X_train.view([input_size, -1, 1])
-    X_test = X_test.view([input_size, -1, 1])
+    X_train = X_train.view([input_time_sequence_size, -1, input_vector_length])
+    X_test = X_test.view([input_time_sequence_size, -1, input_vector_length])
 
     #####################
     # Train model
