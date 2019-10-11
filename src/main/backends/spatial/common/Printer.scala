b059adb20a1c8fc2bc426ffc11716d36f1471c88
Naums Mogers <naums.mogers@arm.com>
2019-10-11 18:48:01 +0100

    Populated the Printer with secondary AST node printing (arithmetic expressions, array addressors etc). Some of the formats are still C-like and require further work.

--- src/main/backends/spatial/common/Printer.scala
+++ src/main/backends/spatial/common/Printer.scala
@@ -2,7 +2,9 @@ package backends.spatial.common
 
 import _root_.ir.Type
 import _root_.ir.ScalarType
-import lift.arithmetic.{ArithExpr, Cst, Predicate}
+import backends.spatial.accel.generator.NotPrintableExpression
+import ir.view.{AccessVar, ArrayAddressor, CastedPointer, Index, Slice}
+import lift.arithmetic.{ArithExpr, BitwiseAND, BitwiseXOR, Cst, IntDiv, LShift, Log, Lookup, Mod, Pow, Predicate, Prod, Sum, Var}
 
 object Printer {
 
@@ -16,11 +18,57 @@ object Printer {
   def toString(e: ArithExpr): String = {
     e match {
       case Cst(c)                                  => c.toString
-      case _                                       => throw new NotImplementedError()
+      case Pow(b, ex)                              =>
+        "(int)pow((float)" + toString(b) + ", " + toString(ex) + ")"
+      case Log(b, x)                               => "(int)log" + b + "((float)" + toString(x) + ")"
+      case Prod(es)                                =>
+        val (denTerms, numTerms) = es.partition({
+          case Pow(_, Cst(-1)) => true
+          case _               => false
+        })
+        val num = toString(numTerms)
+        if (denTerms.isEmpty) s"($num)"
+        else {
+          val den = toString(denTerms.map({
+            case Pow(x, Cst(-1)) => x
+            case _               => throw new IllegalArgumentException()
+          }))
+          s"(($num)/($den))"
+        }
+      case Sum(es)                                 => "(" + es.map(toString).reduce(_ + " + " + _) + ")"
+      case Mod(a, n)                               => "(" + toString(a) + " % " + toString(n) + ")"
+      case AccessVar(array, idxs, _, _)            => s"${toString(array)}(" + idxs.foldLeft("")(_ + toString(_)) + "]"
+      case CastedPointer(v, ty, ofs, addressSpace, _) =>
+        val offset = if (ofs == Cst(0)) "" else s" + ${toString(ofs)}"
+        s"(($addressSpace ${Type.name(ty)}*)(${toString(v)}$offset))"
+      case v: Var                                  => v.toString
+      case IntDiv(n, d)                            => "(" + toString(n) + " / " + toString(d) + ")"
+      case lu: Lookup                              => "lookup" + lu.id + "(" + toString(lu.index) + ")"
+      case BitwiseXOR(a, b)                        => "(" + toString(a) + "^" + toString(b) + ")"
+      case BitwiseAND(a, b)                        => "(" + toString(a) + "&" + toString(b) + ")"
+      case LShift(a, b)                            => "(" + toString(a) + " << " + toString(b) + ")"
+      case i: lift.arithmetic.IfThenElse           =>
+        s"( (${toString(i.test.lhs)} ${i.test.op} ${toString(i.test.rhs)}) ? " +
+          s"${toString(i.t)} : ${toString(i.e)} )"
+      case _                                       => throw new NotPrintableExpression(e.toString)
     }
   }
 
+  def toString(terms: Seq[ArithExpr]): String = {
+    val res = terms.foldLeft("1")((s, e) => s + " * " + toString(e))
+    if (terms.isEmpty) "1"
+    else res.drop(4) // Drop "1 * "
+  }
+
   def toString(p: Predicate): String = {
-    throw new NotImplementedError()
+    s"(${toString(p.lhs)} ${p.op} ${toString(p.rhs)})"
+  }
+
+  def toString(addr: ArrayAddressor): String= {
+    addr match {
+      case Index(idx)               => toString(idx)
+      case Slice(start, step, end)  => toString(start) + " :: " + toString(step) + " :: " + toString(end)
+      case _                        => throw new NotPrintableExpression(addr.toString)
+    }
   }
 }
