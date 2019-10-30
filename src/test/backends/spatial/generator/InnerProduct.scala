54a9d3c1c7bb1f01eac6fede4ce465ee82a8f31c
Naums Mogers <naums.mogers@arm.com>
2019-10-30 11:51:53 +0000

    Improved the DotProduct regression test by cleaning up variable names before the comparison. See code comment for details

--- src/test/backends/spatial/generator/InnerProduct.scala
+++ src/test/backends/spatial/generator/InnerProduct.scala
@@ -10,6 +10,8 @@ import opencl.executor.TestWithExecutor
 import org.junit.Assert._
 import org.junit.Test
 
+import scala.util.matching.Regex
+
 object InnerProduct extends TestWithExecutor
 
 class InnerProduct {
@@ -119,9 +121,33 @@ class InnerProduct {
     assertEquals(gold, count.toFloat, 0.001f)
   }
 
+  /**
+   * Cleans the Spatial program by replacing the numeric in variable names with letters
+   * in increasing alphabetic order. The letters are assigned to variables in their order
+   * of occurrence in the program, thus replacing the ordering of the original numeric ids, which
+   * is affected by the internal mechanics of the compiler. Example:
+   * { val v_someName_37 = 1
+   *   v_someName_37 + v__2
+   * } is replaced with
+   * { val v_someName_b = 1
+   *   v_someName_b + v__a
+   * }
+   */
   def cleanSpatialProgram(code: String): String = {
-    val regexVarIds = "_[0-9]+(__[0-9]+(_[0-9]+)*)*".r
-    regexVarIds.replaceAllIn(code, "")
+    val regexVarNames = new Regex("""(v_(.*?)_[0-9]+(__[0-9]+(_[0-9]+)*)*)""", "varFullName", "varCoreName")
+    val varSubstitutions = regexVarNames.
+      findAllMatchIn(code).
+      map(m => (m.group("varFullName"), m.group("varCoreName"))).
+      toList.distinct.
+      zipWithIndex.map {
+        case ((varFullName: String, varCoreName: String), orderId: Int) =>
+          if (!varCoreName.isEmpty) (varFullName, "v_" + varCoreName + "_"  + (97+orderId).toChar)
+          else (varFullName, "v__" + (97+orderId).toChar)
+      }
+    val updatedCode = varSubstitutions.foldLeft(code) {
+      case (partUpdatedCode, varSubst) => partUpdatedCode.replaceAllLiterally(varSubst._1, varSubst._2)
+    }
+    updatedCode
   }
 
   @Test
