3eca4760a3b9a95731c1f86f81a52089aff9c781
Naums Mogers <naums.mogers@arm.com>
2019-12-05 18:11:21 +0000

    A small fix of the LSTM expression and a test

--- src/test/backends/spatial/generator/LSTM.scala
+++ src/test/backends/spatial/generator/LSTM.scala
@@ -10,6 +10,7 @@ import ir.ast.debug.{AssertType, PrintType}
 import ir.ast.{Drop, Get, Lambda, Let, SkipW, Split, Tuple, Unzip, UserFun, Value, Zip, fun}
 import ir.{ArrayType, TupleType}
 import lift.arithmetic.{NewFactorizationOfSum, SizeVar}
+import org.junit.Assert.assertEquals
 import org.junit.{AfterClass, Test}
 
 object LSTM {
@@ -158,7 +159,7 @@ class LSTM {
                           })) :>> Unzip() :>>
                           fun(mapAccumBodyResult => {
                             val newC = Get(mapAccumBodyResult, 0)
-                            val newH = toDRAM(id1D) o SkipW(left=d, right=0) $ Get(mapAccumBodyResult, 1)
+                            val newH = SkipW(left=d, right=0) o toDRAM(id1D) $ Get(mapAccumBodyResult, 1)
                             Tuple(newC, newH)
                           }) :>>
                           AssertType(TupleType(ArrayType(Float, h), ArrayType(Float, d + h)),
@@ -169,7 +170,7 @@ class LSTM {
                 fun(mapAccumResult => {
                   val newCs = toDRAM(id1D) $ Get(mapAccumResult, 0)
                   val newHX = Get(mapAccumResult, 1) :>>
-                    AssertType(ArrayType(ArrayType(Float, d+ h), nSteps), "Updated h (with an offset)") :>>
+                    AssertType(ArrayType(ArrayType(Float, d + h), nSteps), "Updated h (with an offset)") :>>
                     PrintType() :>>
                     JoinW() :>> PrintType() :>>
                     SkipW(left=h, right=0) :>>
@@ -210,6 +211,159 @@ class LSTM {
 
     val generatedSpatial = spatial.common.RuntimeCompiler(runTimeLambda)
     println(generatedSpatial)
+
+    val expectedOutCode =
+      """|{
+         |    def id_3(x: SRAM1[Float]): SRAM1[Float] = {
+         |        x
+         |    }
+         |    def id_2(x: Float): Float = {
+         |        x
+         |    }
+         |    def id_1(x: DRAM2[Float]): DRAM2[Float] = {
+         |        x
+         |    }
+         |    def id_0(x: DRAM1[Float]): DRAM1[Float] = {
+         |        x
+         |    }
+         |    def mult(l: Float, r: Float): Float = {
+         |        l * r
+         |    }
+         |    def add(x: Float, y: Float): Float = {
+         |        x + y
+         |    }
+         |    // reduce_seq
+         |    // unroll
+         |    val v__75 = SRAM[Float](v_h_3).buffer
+         |    v__75 load id_0(v__57(0::v_h_3))
+         |    val v__76 = SRAM[Float](v_h_3, (v_h_3 + v_d_4))
+         |    v__76 load id_1(v__58(0::v_h_3, 0::(v_h_3 + v_d_4)))
+         |    val v__77 = SRAM[Float](v_h_3, (v_h_3 + v_d_4))
+         |    v__77 load id_1(v__59(0::v_h_3, 0::(v_h_3 + v_d_4)))
+         |    val v__78 = SRAM[Float](v_h_3, (v_h_3 + v_d_4))
+         |    v__78 load id_1(v__60(0::v_h_3, 0::(v_h_3 + v_d_4)))
+         |    val v__79 = SRAM[Float](v_h_3, (v_h_3 + v_d_4))
+         |    v__79 load id_1(v__61(0::v_h_3, 0::(v_h_3 + v_d_4)))
+         |    val v__80 = SRAM[Float](v_h_3)
+         |    v__80 load id_0(v__62(0::v_h_3))
+         |    val v__81 = SRAM[Float](v_h_3)
+         |    v__81 load id_0(v__63(0::v_h_3))
+         |    val v__82 = SRAM[Float](v_h_3)
+         |    v__82 load id_0(v__64(0::v_h_3))
+         |    val v__83 = SRAM[Float](v_h_3)
+         |    v__83 load id_0(v__65(0::v_h_3))
+         |    // mapAccum_seq
+         |    Sequential.Foreach(0 until v_nSteps_2 by 1) { (v_i_34) =>
+         |        val v__87 = SRAM[Float]((v_h_3 + v_d_4))
+         |        v__87 load id_0(v__56((v_i_34 * (v_h_3 + v_d_4))::(v_h_3 + v_d_4 + (v_h_3 * v_i_34) + (v_d_4 * v_i_34))))
+         |        val v__147 = SRAM[Float](v_h_3)
+         |        // SpForeach
+         |        Pipe.Foreach(0 until v_h_3 by 1 par v_hu_5) { (v_i_35) =>
+         |            val v__89 = Reg[Float].buffer
+         |            v__89 := id_2(v__80(v_i_35))
+         |            Pipe.Fold(v__89)(0 until (v_h_3 + v_d_4) by v_rv_6 par v_ru_7) { (v_i_36) =>
+         |                val v__93 = Reg[Float].buffer
+         |                v__93 := id_2(0.0f)
+         |                Pipe.Fold(v__93)(0 until v_rv_6 by 1 par v_rv_6) { (v_i_37) =>
+         |                    val v__96_0 = Reg[Float]
+         |                    // map_seq
+         |                    // iteration count is exactly 1, no loop emitted
+         |                    val v_i_38 = Reg[Int](0)
+         |                    v__96_0 := mult(v__76(v_i_35, (v_i_36 + v_i_37)), v__87((v_i_36 + v_i_37)))
+         |                    // end map_seq
+         |                    v__96_0
+         |                } {
+         |                    add(_, _)
+         |                }
+         |                v__93
+         |            } {
+         |                add(_, _)
+         |            }
+         |            val v__101 = Reg[Float].buffer
+         |            v__101 := id_2(v__81(v_i_35))
+         |            Pipe.Fold(v__101)(0 until (v_h_3 + v_d_4) by v_rv_6 par v_ru_7) { (v_i_41) =>
+         |                val v__105 = Reg[Float].buffer
+         |                v__105 := id_2(0.0f)
+         |                Pipe.Fold(v__105)(0 until v_rv_6 by 1 par v_rv_6) { (v_i_42) =>
+         |                    val v__108_0 = Reg[Float]
+         |                    // map_seq
+         |                    // iteration count is exactly 1, no loop emitted
+         |                    val v_i_43 = Reg[Int](0)
+         |                    v__108_0 := mult(v__77(v_i_35, (v_i_41 + v_i_42)), v__87((v_i_41 + v_i_42)))
+         |                    // end map_seq
+         |                    v__108_0
+         |                } {
+         |                    add(_, _)
+         |                }
+         |                v__105
+         |            } {
+         |                add(_, _)
+         |            }
+         |            val v__114 = Reg[Float]
+         |            v__114 := mult(v__89, v__101)
+         |            val v__115 = Reg[Float].buffer
+         |            v__115 := id_2(v__82(v_i_35))
+         |            Pipe.Fold(v__115)(0 until (v_h_3 + v_d_4) by v_rv_6 par v_ru_7) { (v_i_46) =>
+         |                val v__119 = Reg[Float].buffer
+         |                v__119 := id_2(0.0f)
+         |                Pipe.Fold(v__119)(0 until v_rv_6 by 1 par v_rv_6) { (v_i_47) =>
+         |                    val v__122_0 = Reg[Float]
+         |                    // map_seq
+         |                    // iteration count is exactly 1, no loop emitted
+         |                    val v_i_48 = Reg[Int](0)
+         |                    v__122_0 := mult(v__78(v_i_35, (v_i_46 + v_i_47)), v__87((v_i_46 + v_i_47)))
+         |                    // end map_seq
+         |                    v__122_0
+         |                } {
+         |                    add(_, _)
+         |                }
+         |                v__119
+         |            } {
+         |                add(_, _)
+         |            }
+         |            val v__128 = Reg[Float]
+         |            v__128 := mult(v__75(v_i_35), v__115)
+         |            val v__130 = Reg[Float]
+         |            v__130 := add(v__114, v__128)
+         |            val v__131 = Reg[Float]
+         |            v__131 := id_2(v__130)
+         |            v__75(v_i_35) = id_2(v__131)
+         |            val v__133 = Reg[Float].buffer
+         |            v__133 := id_2(v__83(v_i_35))
+         |            Pipe.Fold(v__133)(0 until (v_h_3 + v_d_4) by v_rv_6 par v_ru_7) { (v_i_51) =>
+         |                val v__137 = Reg[Float].buffer
+         |                v__137 := id_2(0.0f)
+         |                Pipe.Fold(v__137)(0 until v_rv_6 by 1 par v_rv_6) { (v_i_52) =>
+         |                    val v__140_0 = Reg[Float]
+         |                    // map_seq
+         |                    // iteration count is exactly 1, no loop emitted
+         |                    val v_i_53 = Reg[Int](0)
+         |                    v__140_0 := mult(v__79(v_i_35, (v_i_51 + v_i_52)), v__87((v_i_51 + v_i_52)))
+         |                    // end map_seq
+         |                    v__140_0
+         |                } {
+         |                    add(_, _)
+         |                }
+         |                v__137
+         |            } {
+         |                add(_, _)
+         |            }
+         |            v__147(v_i_35) = mult(v__131, v__133)
+         |        }
+         |        // end SpForeach
+         |        v__56((v_h_3 + v_d_4 + (v_h_3 * v_i_34) + (v_d_4 * v_i_34))::(v_d_4 + (2 * v_h_3) + (v_h_3 * v_i_34) + (v_d_4 * v_i_34))) store id_3(v__147(0::v_h_3))
+         |    }
+         |    // end mapAccum_seq
+         |    v__57(0::v_h_3) store id_3(v__75(0::v_h_3))
+         |    // end unroll
+         |    // end reduce_seq
+         |}""".stripMargin
+
+
+    val cleanedGeneratedSpatial = cleanSpatialProgram(generatedSpatial)
+    val cleanedExpectedOutCode = cleanSpatialProgram(expectedOutCode)
+
+    assertEquals(cleanedExpectedOutCode, cleanedGeneratedSpatial)
 
     NewFactorizationOfSum.enabled = originalNewFactorizationEnabledStatus
   }
